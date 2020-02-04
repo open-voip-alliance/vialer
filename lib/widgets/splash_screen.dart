aaf1ba5c45d61c756f24db231af338b4e23018c7
Wilko Manger <wilko.manger@wearespindle.com>
2020-02-04 14:41:28 +0100

    Fix formatting

--- lib/widgets/splash_screen.dart
+++ lib/widgets/splash_screen.dart
@@ -6,9 +6,11 @@ class SplashScreen extends StatelessWidget {
   final Color iconColor;
   final Gradient gradient;
 
-  const SplashScreen(
-      {Key key, this.iconColor = Colors.white, this.gradient = VialerTheme
-          .splashScreenGradient,}) : super(key: key);
+  const SplashScreen({
+    Key key,
+    this.iconColor = Colors.white,
+    this.gradient = VialerTheme.splashScreenGradient,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
