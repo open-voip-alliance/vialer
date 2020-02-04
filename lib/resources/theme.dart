cd17e75f4c4e8776059aa4db0c128e084a3b68d5
Wilko Manger <wilko.manger@wearespindle.com>
2020-02-04 15:44:04 +0100

    Remove some colors and change primary one

--- lib/resources/theme.dart
+++ lib/resources/theme.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 final vialerTheme = ThemeData(
   primaryColor: VialerColors.primary,
-  primaryColorDark: VialerColors.primaryDark,
   buttonTheme: ButtonThemeData(
     height: 42,
     shape: RoundedRectangleBorder(
@@ -39,11 +38,7 @@ abstract class VialerTheme {
 }
 
 abstract class VialerColors {
-  static const primary = Color(0xFFDF662E);
-  static const primaryDark = Color(0xFFDE531B);
-  static const primaryDarker = Color(0xFFD45400);
-
-  static const highlight = Color(0xFF3E50B4);
+  static const primary = Color(0xFFFFA257);
 
   static const onboardingGradientStart = Color(0xFFFF8213);
   static const onboardingGradientEnd = Color(0xFFE94E1B);
