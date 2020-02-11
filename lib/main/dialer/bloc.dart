3cc8cd16dee949131270d0be8a692e4594c27aa1
Wilko Manger <wilko.manger@wearespindle.com>
2020-02-11 10:14:55 +0100

    Don't check permisson when making a call

--- lib/main/dialer/bloc.dart
+++ lib/main/dialer/bloc.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 
 import 'package:android_intent/android_intent.dart';
 import 'package:bloc/bloc.dart';
-import 'package:permission_handler/permission_handler.dart';
 
 import 'event.dart';
 import 'state.dart';
@@ -18,8 +17,6 @@ class DialerBloc extends Bloc<DialerEvent, DialerState> {
   Stream<DialerState> mapEventToState(DialerEvent event) async* {
     if (event is Call) {
       if (Platform.isAndroid) {
-        await PermissionHandler().requestPermissions([PermissionGroup.phone]);
-
         final intent = AndroidIntent(
           action: 'android.intent.action.CALL',
           data: 'tel:${event.phoneNumber}',
