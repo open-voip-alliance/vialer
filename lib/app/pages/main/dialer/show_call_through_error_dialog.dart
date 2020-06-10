90b1ee7142812e4d2197de940837423046acb708
Wilko Manger <wilko.manger@wearespindle.com>
2020-06-10 17:02:36 +0200

    Use specific dialog on iOS & pop confirm page

--- lib/app/pages/main/dialer/show_call_through_error_dialog.dart
+++ lib/app/pages/main/dialer/show_call_through_error_dialog.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-import '../../../resources/localizations.dart';
-
 import '../../../../domain/entities/call_through_exception.dart';
+
+import 'confirm/page.dart';
+
+import '../../../resources/localizations.dart';
+import '../../../resources/theme.dart';
 
 Future<void> showCallThroughErrorDialog(
   BuildContext context,
@@ -20,26 +23,47 @@ Future<void> showCallThroughErrorDialog(
     context: context,
     barrierDismissible: true,
     builder: (context) {
-      return AlertDialog(
-        title: Text(context.msg.main.callThrough.error.title),
-        content: SingleChildScrollView(
-          child: Text(message),
-        ),
-        actions: <Widget>[
-          FlatButton(
-            child: Text(
-              context.msg.generic.button.ok.toUpperCase(),
-              style: TextStyle(
-                color: Theme.of(context).primaryColor,
-              ),
-            ),
-            splashColor: Theme.of(context).primaryColorLight,
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-          ),
-        ],
+      final title = Text(context.msg.main.callThrough.error.title);
+      final content = SingleChildScrollView(
+        child: Text(message),
       );
+
+      final buttonText = context.msg.generic.button.ok;
+      void buttonOnPressed() {
+        Navigator.pop(context);
+        // Also pop the confirm if it's there
+        Navigator.popUntil(context, (route) => route is! ConfirmPageRoute);
+      }
+
+      if (context.isIOS) {
+        return CupertinoAlertDialog(
+          title: title,
+          content: content,
+          actions: <Widget>[
+            CupertinoButton(
+              onPressed: buttonOnPressed,
+              child: Text(buttonText),
+            )
+          ],
+        );
+      } else {
+        return AlertDialog(
+          title: title,
+          content: content,
+          actions: <Widget>[
+            FlatButton(
+              onPressed: buttonOnPressed,
+              child: Text(
+                buttonText.toUpperCase(),
+                style: TextStyle(
+                  color: Theme.of(context).primaryColor,
+                ),
+              ),
+              splashColor: Theme.of(context).primaryColorLight,
+            ),
+          ],
+        );
+      }
     },
   );
 }
