97cba7d6edf5c8c147346cc3ac87b6ab37aa233d
Wilko Manger <wilko.manger@wearespindle.com>
2020-06-25 16:31:51 +0200

    Sanitize logs

--- lib/data/repositories/logging.dart
+++ lib/data/repositories/logging.dart
@@ -59,11 +59,44 @@ class DataLoggingRepository extends LoggingRepository {
       return null;
     }
 
+    var sanitizedMessage = record.message;
+
+    // Source: https://stackoverflow.com/a/6967885
+    final phoneNumberRegex = RegExp(
+      r'\+(9[976]\d|8[987530]\d|6[987]\d|5[90]\d|42\d|3[875]\d|'
+      r'2[98654321]\d|9[8543210]|8[6421]|6[6543210]|5[87654321]|'
+      r'4[987654310]|3[9643210]|2[70]|7|1)\d{1,14}$',
+    );
+
+    // Source: https://emailregex.com/
+    final emailRegex = RegExp(
+      // The first one is not a raw string on purpose, so we can escape the
+      // quotes (and other special characters).
+      // The others aren't so that we don't have to escape anything.
+      '(?:[a-z0-9!#\$%&\'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#\$%&\'*+/=?^_`{|}~-]+)*|'
+      r'"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b'
+      r'\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9]'
+      r'(?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)'
+      r'\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:'
+      r'[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b'
+      r'\x0c\x0e-\x7f])+)\])',
+    );
+
+    // Sanitize the logs, if this ever fires code should be changed to not
+    // make this happen
+    sanitizedMessage = sanitizedMessage
+        .replaceAll(phoneNumberRegex, '[REDACTED PHONE NUMBER]')
+        .replaceAll(emailRegex, '[REDACTED EMAIL]');
+
+    // If log message is the same as the sanitized message, no sanitization
+    // has taken place and we're good
+    assert(record.message == sanitizedMessage);
+
     final user = _authRepository.currentUser;
     final uuid = user != null ? '${user.uuid} ' : '';
 
     return '[${record.time}] ${record.level.name} $uuid${record.loggerName}:'
-        ' ${record.message}';
+        ' $sanitizedMessage';
   }
 
   @override
