305c180040c8572cceccbcd4e4986a09dedb826c
Wilko Manger <wilko.manger@wearespindle.com>
2020-10-01 14:06:57 +0200

    Show API error if needed

--- lib/app/pages/main/widgets/caller/widget.dart
+++ lib/app/pages/main/widgets/caller/widget.dart
@@ -1,7 +1,12 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../../../domain/entities/call_through_exception.dart';
 import '../../../../../domain/entities/survey/survey_trigger.dart';
+
+import '../../../../resources/localizations.dart';
+import '../../../../resources/theme.dart';
 
 import '../../survey/dialog.dart';
 import '../../dialer/confirm/page.dart';
@@ -68,6 +73,13 @@ class _CallerState extends State<Caller> with WidgetsBindingObserver {
         trigger: SurveyTrigger.afterThreeCallThroughCalls,
       );
     }
+
+    if (state is InitiatingCallFailed) {
+      await _showCallThroughErrorDialog(
+        widget.navigatorKey.currentContext,
+        state.exception,
+      );
+    }
   }
 
   @override
@@ -84,4 +96,65 @@ class _CallerState extends State<Caller> with WidgetsBindingObserver {
 
     WidgetsBinding.instance.removeObserver(this);
   }
+}
+
+Future<void> _showCallThroughErrorDialog(
+  BuildContext context,
+  CallThroughException exception,
+) {
+  String message;
+  if (exception is InvalidDestinationException ||
+      exception is NormalizationException) {
+    message = context.msg.main.callThrough.error.invalidDestination;
+  } else {
+    message = context.msg.main.callThrough.error.unknown;
+  }
+
+  return showDialog<void>(
+    context: context,
+    barrierDismissible: true,
+    builder: (context) {
+      final title = Text(context.msg.main.callThrough.error.title);
+      final content = SingleChildScrollView(
+        child: Text(message),
+      );
+
+      final buttonText = context.msg.generic.button.ok;
+      void buttonOnPressed() {
+        Navigator.pop(context);
+        // Also pop the confirm if it's there
+        Navigator.popUntil(context, (route) => route is! ConfirmPageRoute);
+      }
+
+      if (context.isIOS) {
+        return CupertinoAlertDialog(
+          title: title,
+          content: content,
+          actions: <Widget>[
+            CupertinoButton(
+              onPressed: buttonOnPressed,
+              child: Text(buttonText),
+            )
+          ],
+        );
+      } else {
+        return AlertDialog(
+          title: title,
+          content: content,
+          actions: <Widget>[
+            FlatButton(
+              onPressed: buttonOnPressed,
+              child: Text(
+                buttonText.toUpperCase(),
+                style: TextStyle(
+                  color: Theme.of(context).primaryColor,
+                ),
+              ),
+              splashColor: Theme.of(context).primaryColorLight,
+            ),
+          ],
+        );
+      }
+    },
+  );
 }
