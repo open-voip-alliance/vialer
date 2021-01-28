82c5d7c709601b52ca74c4a9cb7a00a32fff6ba3
Marco Vellinga <marco@vellinga.dev>
2021-01-28 10:54:02 +0100

    Redacted some auto collected segment properties

--- lib/domain/repositories/metrics.dart
+++ lib/domain/repositories/metrics.dart
@@ -3,6 +3,18 @@ import 'package:flutter_segment/flutter_segment.dart';
 import '../../app/util/debug.dart';
 
 class MetricsRepository {
+  MetricsRepository() {
+    // Ensure some sensitive tracking stuff is always redacted.
+    Segment.setContext({
+      'ip': '0.0.0.0',
+      'device': {
+        'id': '',
+        'advertisingId': '',
+        'token': '',
+      },
+    });
+  }
+
   Future<void> identify(String userId) async {
     await doIfNotDebug(() async {
       await Segment.identify(userId: userId);
