e6a8f61f53ed435ca8d63a289ae74279e4ed924d
Marco Vellinga <marco@vellinga.dev>
2021-01-29 11:30:35 +0100

    Added brand to segment identify call

--- lib/domain/usecases/metrics/identify_for_tracking.dart
+++ lib/domain/usecases/metrics/identify_for_tracking.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import '../../../dependency_locator.dart';
+import '../../entities/brand.dart';
 import '../../repositories/auth.dart';
 import '../../repositories/metrics.dart';
 import '../../use_case.dart';
@@ -8,10 +9,14 @@ import '../../use_case.dart';
 class IdentifyForTrackingUseCase extends FutureUseCase<void> {
   final _metricsRepository = dependencyLocator<MetricsRepository>();
   final _authRepository = dependencyLocator<AuthRepository>();
+  final _brand = dependencyLocator<Brand>();
 
   @override
   Future<void> call() async {
     assert(_authRepository.currentUser != null);
-    await _metricsRepository.identify(_authRepository.currentUser.uuid);
+    await _metricsRepository.identify(
+      _authRepository.currentUser.uuid,
+      _brand.identifier,
+    );
   }
 }
