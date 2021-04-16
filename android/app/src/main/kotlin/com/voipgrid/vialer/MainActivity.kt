8d9c4ee0dc2874de1cb0459e8a94a720a54ce5fc
Jeremy Norman <jeremy.norman@wearespindle.com>
2021-04-16 13:33:00 +0000

    Resolve "Incoming calls: Screen remains off when receiving a call"

--- android/app/src/main/kotlin/com/voipgrid/vialer/MainActivity.kt
+++ android/app/src/main/kotlin/com/voipgrid/vialer/MainActivity.kt
@@ -1,19 +1,29 @@
 package com.voipgrid.vialer
 
-import android.os.Bundle
-import androidx.annotation.NonNull;
+import android.os.Build
+import android.view.WindowManager
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
-import org.openvoipalliance.flutterphonelib.addPhoneLibCallScreenObserver
 
 class MainActivity : FlutterActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        addPhoneLibCallScreenObserver()
-    }
 
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine)
+
+        // The FlutterActivity seems to behave differently to regular Android activities
+        // and adding this worked in situations where using the PIL's CallScreenLifecycleObserver
+        // did not.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
+            setShowWhenLocked(true)
+            setTurnScreenOn(true)
+        } else {
+            window.addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
+                    or WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD
+                    or WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+                    or WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
+                    or WindowManager.LayoutParams.FLAG_ALLOW_LOCK_WHILE_SCREEN_ON)
+        }
     }
 }
