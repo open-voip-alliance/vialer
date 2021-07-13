89bcc326f5b6a8fa2dca28caa7ec4bea3277ea44
Jeremy Norman <jeremy.norman@wearespindle.com>
2021-07-13 14:23:21 +0000

    Resolve "Recents: All calls/Missed calls differentiation"

--- lib/app/pages/main/widgets/header.dart
+++ lib/app/pages/main/widgets/header.dart
@@ -2,13 +2,18 @@ import 'package:flutter/material.dart';
 
 class Header extends StatelessWidget {
   final String data;
+  final EdgeInsets padding;
 
-  const Header(this.data, {Key? key}) : super(key: key);
+  const Header(
+    this.data, {
+    Key? key,
+    this.padding = const EdgeInsets.only(bottom: 8),
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(bottom: 8),
+      padding: padding,
       child: Text(
         data,
         style: TextStyle(
