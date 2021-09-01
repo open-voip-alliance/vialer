0b531eca9d59d67403f42fe6c2dbe8a64604dd5b
Wilko Manger <wilko.manger@wearespindle.com>
2021-09-01 10:26:33 +0200

    Add missed call notifications

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -4,15 +4,21 @@ import flutter_phone_lib
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    let registerPlugins = GeneratedPluginRegistrant.register
-    registerPlugins(self)
+    override func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        addOnMissedCallNotificationPressedDelegate()
 
-    startPhoneLib(registerPlugins)
+        return super.application(application, willFinishLaunchingWithOptions: launchOptions)
+    }
 
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        let registerPlugins = GeneratedPluginRegistrant.register
+        registerPlugins(self)
+
+        startPhoneLib(registerPlugins)
+
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
 }
