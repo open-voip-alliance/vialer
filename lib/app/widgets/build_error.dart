9761ea952f08b4b9bd4a17522de84b10c6391994
Wilko Manger <wilko.manger@wearespindle.com>
2021-10-13 11:49:02 +0200

    Show build errors in a nicer way

---
+++ lib/app/widgets/build_error.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import '../resources/localizations.dart';
+import '../util/brand.dart';
+
+class BuildError extends StatelessWidget {
+  const BuildError({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      context.msg.buildError.anErrorOccurred,
+      textScaleFactor: 1,
+      textAlign: TextAlign.center,
+      style: TextStyle(
+        fontSize: 14,
+        color: context.brand.theme.red1,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+  }
+}
