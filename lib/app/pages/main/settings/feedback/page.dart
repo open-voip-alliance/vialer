e8b81e7028d04ee52b4babcd343390335866f6eb
Wilko Manger <wilko.manger@wearespindle.com>
2021-11-17 16:26:13 +0100

    Remove beta references from strings

--- lib/app/pages/main/settings/feedback/page.dart
+++ lib/app/pages/main/settings/feedback/page.dart
@@ -103,7 +103,7 @@ class _FeedbackPageState extends State<FeedbackPage> {
                             onPressed: state is FeedbackNotSent
                                 ? () =>
                                     context.read<FeedbackCubit>().sendFeedback(
-                                          title: 'Beta Feedback',
+                                          title: 'Feedback',
                                           text: _textController.text,
                                         )
                                 : null,
@@ -157,7 +157,7 @@ class _FeedbackFormHeader extends StatelessWidget {
             ),
             Flexible(
               child: Text(
-                context.msg.main.settings.feedback.header(
+                context.msg.main.settings.feedback.callout(
                   context.brand.appName,
                 ),
               ),
