583b72492e77d08e4f7ec53f2d6e06b9b9097d7d
Jeremy Norman <jeremy.norman@wearespindle.com>
2021-11-19 16:43:24 +0100

    Resolve "Create Codemagic workflows for production for each brand"

---
+++ utils/ci/codemagic/prepare_release_notes.dart
@@ -0,0 +1,163 @@
+import 'dart:io';
+import 'package:dartx/dartx.dart';
+import 'package:path/path.dart';
+
+/// A mapping between the supported language and the language codes used by the
+/// app/play store. e.g. dutch.txt will produce nl_NL.txt and nl.txt
+const localizationMap = {
+  'english.txt': [
+    'en-US',
+  ],
+  'dutch.txt': [
+    'nl-NL',
+    'nl',
+  ],
+};
+
+const rawReleaseNotesPath = 'release_notes';
+
+/// The maximum number of characters supported by the store-fronts, this check
+/// will be performed here as soon as possible so we don't risk partially
+/// publishing.
+const maxAllowedCharactersInReleaseNotes = 500;
+
+/// Finds the appropriate release notes for the current tag and moves the files
+/// into the root project directory for Codemagic to publish.
+///
+/// Expected release notes location is: release_notes/TAG/BRAND
+/// e.g. release_notes/v7.0.0/vialer
+///
+/// By default this will fallback to VIALER if there are no release notes
+/// provided for the specified brand.
+///
+/// The tag and brand information must either be passed to the script via
+/// command line arguments:
+///
+/// e.g. dart prepare_release_notes.dart v7.0.0 vialer
+///
+/// or if these aren't present the env vars set in the Codemagic environment
+/// will be used.
+Future<void> main(List<String> args) async {
+  final tag = args.elementAtOrNull(0) ?? Platform.environment['FCI_TAG'];
+  final brand = args.elementAtOrNull(1) ?? Platform.environment['BRAND'];
+
+  if (tag == null || brand == null) {
+    throw ArgumentError(
+      'You must provide a valid tag and brand. '
+      'This can be done as arguments or via FCI_TAG/BRAND env vars.',
+    );
+  }
+
+  final directory = await findReleaseNotesDirectory(
+    tag: tag,
+    brand: brand,
+  );
+
+  final files = directory.list();
+
+  if (await files.length <= 0) {
+    throw Exception('There are no files in the release notes directory');
+  }
+
+  await for (var file in files) {
+    if (file is File) {
+      generateReleaseNotesForCodemagic(file);
+    } else {
+      throw Exception('${file.path} is not a file and should not be here.');
+    }
+  }
+
+  verifyAllReleaseNotesFilesWereGenerated();
+}
+
+/// Finds the mappings and generates the relevant files for each language.
+Future<void> generateReleaseNotesForCodemagic(File file) async {
+  final fileName = basename(file.path);
+  final mapping = localizationMap[fileName];
+
+  if (mapping == null) {
+    throw Exception(
+      '${file.path} is not a valid release notes file, '
+      'remove it from the directory. '
+      'Supported are: ${localizationMap.keys.toList().join(', ')}',
+    );
+  }
+
+  final releaseNotesCharacterCount = await file.readAsString().then(
+        (content) => content.length,
+      );
+
+  if (releaseNotesCharacterCount >= maxAllowedCharactersInReleaseNotes) {
+    throw Exception(
+      '$fileName is $releaseNotesCharacterCount characters long '
+      'exceeding the maximum of $maxAllowedCharactersInReleaseNotes.',
+    );
+  }
+
+  if (releaseNotesCharacterCount <= 0) {
+    throw Exception('Unable to publish these notes as $fileName is empty.');
+  }
+
+  for (var generatedFilePath in mapping) {
+    await file.copy(createCodemagicReleaseNotesFileName(generatedFilePath));
+  }
+}
+
+/// Find the release notes directory. If a fallback brand is provided, the
+/// directory for that brand will be used if the specified brand cannot
+/// be found.
+Future<Directory> findReleaseNotesDirectory({
+  required String tag,
+  required String brand,
+  String? fallbackBrand = 'vialer',
+}) async {
+  final path = '$rawReleaseNotesPath/$tag/$brand';
+
+  final directory = Directory(path);
+
+  if (!await directory.exists()) {
+    if (fallbackBrand == null) {
+      throw Exception(
+        'Unable to find release notes directory ($path) '
+        'make sure it has been created for this tag.',
+      );
+    }
+
+    return findReleaseNotesDirectory(
+      tag: tag,
+      brand: brand,
+      fallbackBrand: null,
+    );
+  }
+
+  return directory;
+}
+
+Future<void> verifyAllReleaseNotesFilesWereGenerated() async {
+  final expectedFiles = localizationMap.values.expand((element) => element);
+
+  for (var expectedFile in expectedFiles) {
+    final expectedFilePath = createCodemagicReleaseNotesFileName(expectedFile);
+
+    final file = await File(expectedFilePath);
+
+    if (!await file.exists()) {
+      throw Exception(
+        'Unable to find expected file $expectedFilePath, something went wrong.',
+      );
+    }
+  }
+}
+
+/// Creates the file name expected by Codemagic for the release notes files.
+String createCodemagicReleaseNotesFileName(String name) {
+  /// The prefix that Codemagic expects to be at the front of the release notes
+  /// file. e.g. release_notes_en_US.txt
+  const generatedFilePrefix = 'release_notes_';
+
+  /// The file extension that Codemagic is expecting the release notes files
+  /// to all have.
+  const generatedFileExtension = '.txt';
+
+  return '$generatedFilePrefix$name$generatedFileExtension';
+}
---
+++ utils/generate_release_notes_template.dart
@@ -0,0 +1,69 @@
+import 'dart:io';
+
+import 'package:dartx/dartx.dart';
+
+import 'ci/codemagic/prepare_release_notes.dart';
+
+
+final brandsToGenerateFor = [
+  'vialer',
+  'voys',
+];
+
+final validReleasePattern = RegExp(r'^v[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$');
+
+/// A simple helper to generate the release notes template files for a specific
+/// release.
+///
+/// If you are on a release branch the correct version will be automatically
+/// picked up:
+///
+/// `dart utils/local/generate_release_notes_template.dart`
+///
+/// If you are not on a release branch you must specific a version:
+///
+/// `dart utils/local/generate_release_notes_template.dart v7.1.1`
+Future<void> main(List<String> args) async {
+  final release = await findReleaseName(args);
+
+  if (!validReleasePattern.hasMatch(release)) {
+    throw ArgumentError('$release does not look like a valid release,'
+        ' expecting e.g. v7.1.1');
+  }
+
+  for (var fileName in localizationMap.keys) {
+    for (var brand in brandsToGenerateFor) {
+      final path = '$rawReleaseNotesPath/$release/$brand/$fileName';
+      await File(path).create(recursive: true);
+      print('Created $path');
+    }
+  }
+}
+
+Future<String> findReleaseName(List<String> args) async {
+  final release = args.elementAtOrNull(0);
+
+  if (release != null) return release;
+
+  final branch = await currentGitBranch;
+
+  if (!branch.startsWith('release/')) {
+    throw ArgumentError(
+      'No release has been provided and you are'
+      ' not on a release branch e.g. release/v7.1.1',
+    );
+  }
+
+  return branch.replaceFirst('release/', '').trim();
+}
+
+Future<String> get currentGitBranch async => Process.run(
+      'git',
+      [
+        'rev-parse',
+        '--abbrev-ref',
+        'HEAD',
+      ],
+    ).then(
+      (value) => value.stdout as String,
+    );
