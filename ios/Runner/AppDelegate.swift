0e38975ae333899363da05b51769b4ca64496d3d
Jeremy Norman <jeremy.norman@wearespindle.com>
2022-03-28 11:01:11 +0200

    Added inbound call metrics for ios

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -5,7 +5,9 @@ import flutter_phone_lib
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
     private let logger = Logger()
-    
+    private let flutterSharedPreferences = FlutterSharedPreferences()
+    private var segment: Segment?
+
     override func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         addOnMissedCallNotificationPressedDelegate()
 
@@ -18,18 +20,37 @@ import flutter_phone_lib
     ) -> Bool {
         let registerPlugins = GeneratedPluginRegistrant.register
         registerPlugins(self)
-        
-        let middleware = Middleware(logger: self.logger) 
 
-        startPhoneLib(registerPlugins, nativeMiddleware: middleware) { message, level in
+        if (segment == nil) {
+            segment = Segment(logger: self.logger, prefs: self.flutterSharedPreferences)
+            segment?.initialize()
+        }
+
+        let middleware = Middleware(logger: self.logger, segment: self.segment!)
+
+        startPhoneLib(
+            registerPlugins,
+            nativeMiddleware: middleware,
+            onCallEnded: { (call) in
+                self.segment?.track(event: "voip-call-ended", properties: [
+                    "call_id" : middleware.currentCallInfo?.callId ?? "",
+                    "correlation_id" : middleware.currentCallInfo?.correlationId ?? "",
+                    "push_received_time" : middleware.currentCallInfo?.pushReceivedTime ?? "",
+                    "reason" : call.reason,
+                    "direction" : call.direction,
+                    "duration" : call.duration,
+                    "mos" : call.mos,
+                ])
+
+                middleware.currentCallInfo = nil
+            }
+        ) { message, level in
             self.logger.writeLog(message)
         }
 
         let controller: FlutterViewController = window?.rootViewController as! FlutterViewController
-        NativeLoggingSetup(controller.binaryMessenger, logger)
         ContactSortHostApiSetup(controller.binaryMessenger, ContactSortApi())
-        NativeMetricsSetup(controller.binaryMessenger, Metrics())
-        CallScreenBehaviorSetup(controller.binaryMessenger, CallScreenBehaviorApi())
+        NativeLoggingSetup(controller.binaryMessenger, logger)
         
         return super.application(application, didFinishLaunchingWithOptions: launchOptions)
     }
