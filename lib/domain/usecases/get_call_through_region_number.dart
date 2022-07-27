d1c9e4b5af002d49e3d549d98189ed552fadce52
Jeremy Norman <jeremy.norman@wearespindle.com>
2022-07-27 11:28:57 +0200

    Trying more numbers to find a call-through number and throwing better errors/tracking when it fails

--- lib/domain/usecases/get_call_through_region_number.dart
+++ lib/domain/usecases/get_call_through_region_number.dart
@@ -4,12 +4,14 @@ import '../../../dependency_locator.dart';
 import '../entities/exceptions/call_through.dart';
 import '../repositories/call_through.dart';
 import '../repositories/memory_storage_repository.dart';
+import '../repositories/metrics.dart';
 import '../use_case.dart';
 import 'get_user.dart';
 
 class GetCallThroughRegionNumberUseCase extends UseCase {
   final _memoryStorageRepository = dependencyLocator<MemoryStorageRepository>();
   final _callThroughRepository = dependencyLocator<CallThroughRepository>();
+  final _metricsRepository = dependencyLocator<MetricsRepository>();
 
   final _getUser = GetUserUseCase();
 
@@ -25,10 +27,17 @@ class GetCallThroughRegionNumberUseCase extends UseCase {
         throw CallThroughException();
       }
 
-      regionNumber = await _callThroughRepository.retrieveRegionNumber(
-        destination,
-        user: user,
-      );
+      try {
+        regionNumber = await _callThroughRepository.retrieveRegionNumber(
+          destination,
+          user: user,
+        );
+      } on CallThroughException catch (e) {
+        _metricsRepository.track('call-through-region-number-failed', {
+          'error': e.runtimeType.toString(),
+        });
+        rethrow;
+      }
 
       _memoryStorageRepository.regionNumber = regionNumber;
     }
