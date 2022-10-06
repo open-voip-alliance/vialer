ff5d380efa57a944a74821393c75fd13328b89a2
Jeremy Norman <jeremy.norman@wearespindle.com>
2022-10-06 11:24:36 +0100

    Temporarily removing contact import to test Google's review

--- android/app/src/main/kotlin/com/voipgrid/vialer/ContactImporter.kt
+++ android/app/src/main/kotlin/com/voipgrid/vialer/ContactImporter.kt
@@ -28,87 +28,10 @@ class ContactImporter(context: Context) : Pigeon.Contacts {
     override fun fetchContacts(
         result: Pigeon.Result<List<Pigeon.PigeonContact>>?,
     ) {
-        CoroutineScope(Dispatchers.IO).launch {
-            val results = contacts
-                .broadQuery()
-                .find()
-                .map {
-                    Pigeon.PigeonContact.Builder().apply {
-                        setChosenName(it.displayNamePrimary)
-                        setCompany(it.organizations().firstOrNull()?.company)
-                        setFamilyName(it.rawContacts.firstOrNull()?.name?.familyName)
-                        setIdentifier(it.id.toString())
-                        setGivenName(it.rawContacts.firstOrNull()?.name?.givenName)
-                        setMiddleName(it.rawContacts.firstOrNull()?.name?.middleName)
-                        setPhoneNumbers(it.phoneList().toPhoneItems())
-                        setEmails(it.emailList().toEmailItems())
-                    }.build()
-                }
-
-
-            result?.success(results)
-        }
+        result?.success(listOf())
     }
 
     override fun importContactAvatars(avatarDirectoryPath: String, result: Pigeon.Result<Void>?) {
-        CoroutineScope(Dispatchers.IO).launch {
-            val results = contacts
-                .query()
-                .where { Contact.PhotoThumbnailUri.isNotNullOrEmpty() }
-                .include(Fields.Contact.PhotoThumbnailUri)
-                .find()
-                .shuffled()
-                .onEach {
-                    it.photoThumbnailBytes(contacts)?.let { bytes ->
-                        File(avatarDirectoryPath.avatarPath(it)).writeBytes(bytes)
-                    }
-                }
-
-            launch {
-                removeOrphanedAvatars(
-                    avatarDirectoryPath,
-                    results.map { it.id.toString() }
-                )
-            }
-
-            result?.success(null)
-        }
+        result?.success(null)
     }
-
-    private fun removeOrphanedAvatars(avatarDirectoryPath: String, validIds: List<String>) =
-        File(avatarDirectoryPath).listFiles()
-            ?.filter { !validIds.contains(it.nameWithoutExtension) }
-            ?.forEach { it.delete() }
 }
-
-fun List<Phone>.toPhoneItems() = filter { phone -> phone.number?.isNotBlank() == true }
-        .map { phone -> Pigeon.PigeonContactItem.Builder().apply {
-            setLabel(phone.displayLabel)
-            setValue(phone.number!!)
-        }.build() }
-        .distinctBy { item -> item.value?.replace(Regex("[^0-9]"), "") }
-
-fun List<Email>.toEmailItems() = filter { email -> email.primaryValue?.isNotBlank() == true }
-    .map { email -> Pigeon.PigeonContactItem.Builder().apply {
-        setLabel(email.displayLabel)
-        setValue(email.primaryValue!!)
-    }.build() }
-    .distinctBy { item -> item.value }
-
-fun String.formatLabel() = toLowerCase(Locale.current).capitalize(Locale.current)
-
-fun String.avatarPath(contact: contacts.core.entities.Contact) = "$this/${contact.id}.jpg"
-
-val Phone.displayLabel: String
-    get() = when {
-        label?.isNotBlank() == true -> label!!.formatLabel()
-        type != null -> type!!.name.formatLabel()
-        else -> ""
-    }
-
-val Email.displayLabel: String
-    get() = when {
-        label?.isNotBlank() == true -> label!!.formatLabel()
-        type?.name != null -> type?.name!!.formatLabel()
-        else -> ""
-    }
