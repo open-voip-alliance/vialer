f870c45a15af9c0949a8b652feac1e02e6f00acb
Chris Kontos <chris.kontos@wearespindle.com>
2022-10-07 19:15:11 +0200

    1214-create-business-availability-repo

---
+++ lib/domain/repositories/services/business_availability.dart
@@ -0,0 +1,66 @@
+import 'package:chopper/chopper.dart' hide JsonConverter;
+
+import '../../entities/user.dart';
+import '../../usecases/get_brand.dart';
+import 'util.dart';
+
+part 'business_availability.chopper.dart';
+
+@ChopperApi()
+abstract class BusinessAvailabilityService extends ChopperService {
+  static BusinessAvailabilityService create() {
+    final _getBrand = GetBrandUseCase();
+    final _businessAvailabilityBaseUrl =
+        _getBrand.call().businessAvailabilityUrl.toString();
+
+    return _$BusinessAvailabilityService(
+      ChopperClient(
+        baseUrl: _businessAvailabilityBaseUrl,
+        converter: JsonConverter(),
+        interceptors: [
+          const AuthorizationInterceptor(),
+          UnauthorizedResponseInterceptor(),
+        ],
+      ),
+    );
+  }
+
+  static BusinessAvailabilityService createInIsolate({
+    required User user,
+    required String baseUrl,
+  }) {
+    return _$BusinessAvailabilityService(
+      ChopperClient(
+        baseUrl: baseUrl,
+        converter: JsonConverter(),
+        interceptors: [
+          AuthorizationInterceptor(user: user),
+        ],
+      ),
+    );
+  }
+
+  @Get(path: '{client_uuid}/temporary-redirect')
+  Future<Response> getTemporaryRedirect({
+    @Path() String clientUuid,
+  });
+
+  @Post(path: '{client_uuid}/temporary-redirect')
+  Future<Response> setTemporaryRedirect(
+    @Path() String clientUuid,
+    @Body() Map<String, dynamic> body,
+  );
+
+  @Put(path: '{client_uuid}/temporary-redirect/{temporary_redirect_id}')
+  Future<Response> updateTemporaryRedirect(
+    @Path() String clientUuid,
+    @Path() String temporaryRedirectId,
+    @Body() Map<String, dynamic> body,
+  );
+
+  @Delete(path: '{client_uuid}/temporary-redirect/{temporary_redirect_id}')
+  Future<Response> deleteTemporaryRedirect(
+    @Path() String clientUuid,
+    @Path() String temporaryRedirectId,
+  );
+}
