841db5f29580216b27db464f9217499976b481a4
Jeremy Norman <jeremy.norman@wearespindle.com>
2022-10-31 11:03:42 +0000

    Running GetLatestUser as SingleInstanceTask

--- lib/app/util/single_task.dart
+++ lib/app/util/single_task.dart
@@ -1,29 +1,39 @@
 import 'loggable.dart';
 
-typedef Task = Future<void> Function();
-
 /// Wraps a task that should only be executed once at a time.
-class SingleInstanceTask with Loggable {
+class SingleInstanceTask<T> with Loggable {
   /// A unique identifier for this task, a matching task is what defines
   /// if it is running.
   final String name;
 
-  /// The list of running tasks, if the name of this task appears in the list
+  /// The map of running tasks, if the name of this task is in this map
   /// it means it is running.
-  static final _runningTasks = <String>[];
+  ///
+  /// The name maps to a future which is the actual logic of the running
+  /// task.
+  static final _runningTasks = <String, Future>{};
 
   SingleInstanceTask._(this.name);
 
   factory SingleInstanceTask.named(String name) => SingleInstanceTask._(name);
 
-  Future<void> run(Task task) async {
-    if (_runningTasks.contains(name)) {
+  /// Generate a named task based on the name of the class being passed into,
+  /// this makes it cleaner to generate a [SingleInstanceTask] that matches
+  /// the class you are currently working in.
+  factory SingleInstanceTask.of(dynamic name) => SingleInstanceTask.named(
+        name.runtimeType.toString(),
+      );
+
+  Future<T> run(Future<T> Function() task) async {
+    if (_runningTasks.containsKey(name)) {
       logger.info('Unable to start [$name] as it is already running.');
-      return;
+      return _runningTasks[name] as Future<T>;
     }
 
-    _runningTasks.add(name);
-    await task();
+    final future = task();
+    _runningTasks[name] = future;
+    final result = await task();
     _runningTasks.remove(name);
+    return result;
   }
 }
