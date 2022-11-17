32b390b3742ee8e0e2404b4f20d8509632dc2d83
Jeremy Norman <jeremy.norman@wearespindle.com>
2022-11-17 15:16:16 +0000

    Fixed formatting

--- lib/app/pages/main/business_availability/temporary_redirect/explanation.dart
+++ lib/app/pages/main/business_availability/temporary_redirect/explanation.dart
@@ -6,10 +6,19 @@ import '../../../../resources/localizations.dart';
 class TemporaryRedirectExplanation extends StatelessWidget {
   final TemporaryRedirectDestination? currentDestination;
 
+  /// When enabled, will include a hint to select a destination if
+  /// one isn't chosen.
+  final bool enableHint;
+
   const TemporaryRedirectExplanation({
     super.key,
     required this.currentDestination,
+    this.enableHint = true,
   });
+
+  String _voicemailText(BuildContext context) =>
+      currentDestination?.displayName ??
+      context.msg.main.temporaryRedirect.explanation.selectVoicemail;
 
   @override
   Widget build(BuildContext context) {
@@ -21,11 +30,12 @@ class TemporaryRedirectExplanation extends StatelessWidget {
           ),
           if (currentDestination is Voicemail)
             TextSpan(
-              text: '${currentDestination?.displayName} ',
+              text: ' ${_voicemailText(context)} ',
               style: const TextStyle(
                 fontStyle: FontStyle.italic,
               ),
             ),
+          if (currentDestination is Unknown) const TextSpan(text: ' '),
           TextSpan(
             text: context.msg.main.temporaryRedirect.explanation.end,
           ),
