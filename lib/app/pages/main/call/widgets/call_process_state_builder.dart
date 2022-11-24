752800ae1405808f7c1b9805fc27a54928d8a6a0
Jeremy Norman <jeremy.norman@wearespindle.com>
2022-11-24 14:06:22 +0000

    Not building if no voip call, and logging for more info

--- lib/app/pages/main/call/widgets/call_process_state_builder.dart
+++ lib/app/pages/main/call/widgets/call_process_state_builder.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../../util/loggable.dart';
 import '../../widgets/caller/cubit.dart';
 
 /// Only builds when the [CallerCubit]'s state is a [CallProcessState].
-class CallProcessStateBuilder extends StatelessWidget {
+class CallProcessStateBuilder extends StatelessWidget with Loggable {
   final BlocWidgetBuilder<CallProcessState> builder;
 
-  const CallProcessStateBuilder({
+  CallProcessStateBuilder({
     required this.builder,
     Key? key,
   }) : super(key: key);
@@ -15,7 +16,19 @@ class CallProcessStateBuilder extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<CallerCubit, CallerState>(
-      buildWhen: (_, current) => current is CallProcessState,
+      buildWhen: (_, current) {
+        if (current is! CallProcessState) return false;
+
+        if (current.voipCall == null) {
+          logger.warning(
+            'State is ${current.runtimeType}(CallProcessState) but no active '
+                'voip call',
+          );
+          return false;
+        }
+
+        return true;
+      },
       builder: (context, state) => builder(context, state as CallProcessState),
     );
   }
