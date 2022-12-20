306f9defcfab68bfab6f3b23248d9046cdee3f4f
Jeremy Norman <jeremy.norman@wearespindle.com>
2022-12-20 15:14:09 +0000

    Moved location of notification strings

--- android/app/src/main/kotlin/com/voipgrid/vialer/notifications/LoggedInElsewhereNotification.kt
+++ android/app/src/main/kotlin/com/voipgrid/vialer/notifications/LoggedInElsewhereNotification.kt
@@ -1,29 +1,26 @@
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
-import androidx.compose.ui.res.integerResource
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
-import com.voipgrid.vialer.BrandIcons
 import com.voipgrid.vialer.R
-import java.lang.Integer.getInteger
 
 class LoggedInElsewhereNotification(private val context: Context) {
     fun show() = context.apply {
         val channel = NotificationChannel(
             CHANNEL_ID,
-            getString(R.string.logged_in_elsewhere_notification_channel_name),
+            getString(R.string.notification_logged_in_elsewhere_channel_name),
             NotificationManager.IMPORTANCE_HIGH,
         ).apply {
-            description = getString(R.string.logged_in_elsewhere_notification_channel_description)
+            description = getString(R.string.notification_logged_in_elsewhere_channel_description)
         }
 
         getSystemService(NotificationManager::class.java).createNotificationChannel(channel)
 
         val notification = NotificationCompat.Builder(this, CHANNEL_ID)
             .setSmallIcon(R.drawable.ic_service)
-            .setContentTitle(getString(R.string.logged_in_elsewhere_notification_message_title))
-            .setContentText(getString(R.string.logged_in_elsewhere_notification_message_subtitle))
+            .setContentTitle(getString(R.string.notification_logged_in_elsewhere_message_title))
+            .setContentText(getString(R.string.notification_logged_in_elsewhere_message_subtitle))
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
 
         NotificationManagerCompat.from(this).apply {
