8d41c7f5557d626813a9b0e1a24085499a7a7725
Jeremy Norman <jeremy.norman@wearespindle.com>
2022-12-26 10:01:23 +0000

    Convert authentication models to Freezed

--- lib/domain/authentication/unauthorized_api_response.dart
+++ lib/domain/authentication/unauthorized_api_response.dart
@@ -1,8 +1,13 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'unauthorized_api_response.freezed.dart';
+
 /// An unauthorized (typically 401) response was received when attempting to
 /// contact the API.
-class UnauthorizedApiResponseEvent {
-  /// The status code that caused the user to appear as unauthorized.
-  final int statusCode;
-
-  const UnauthorizedApiResponseEvent(this.statusCode);
+@freezed
+class UnauthorizedApiResponseEvent with _$UnauthorizedApiResponseEvent {
+  const factory UnauthorizedApiResponseEvent(
+    /// The status code that caused the user to appear as unauthorized.
+    int statusCode,
+  ) = _UnauthorizedApiResponseEvent;
 }
