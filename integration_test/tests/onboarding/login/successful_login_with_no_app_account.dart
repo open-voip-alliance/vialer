bc3a10a72a50f9ba9307cb663f00bb33ad5b2b0e
Jeremy Norman <jeremy.norman@wearespindle.com>
2023-01-12 15:53:41 +0100

    Waiting on the voicemail screen

--- integration_test/tests/onboarding/login/successful_login_with_no_app_account.dart
+++ integration_test/tests/onboarding/login/successful_login_with_no_app_account.dart
@@ -1,5 +1,4 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:vialer/app/pages/onboarding/info/page.dart';
 import 'package:vialer/app/pages/onboarding/login/page.dart';
 import 'package:vialer/app/pages/onboarding/mobile_number/page.dart';
 import 'package:vialer/app/pages/onboarding/page.dart';
@@ -31,10 +30,4 @@ void main() => runTest([
       await tester.pumpAndSettle(const Duration(seconds: 1));
 
       expect(OnboardingPage.keys.page.currentStep, OnboardingStep.voicemail);
-
-      await tester.tap(find.byKey(InfoPage.keys.continueButton));
-
-      await tester.pumpAndSettle(const Duration(seconds: 1));
-
-      expect(OnboardingPage.keys.page.currentStep, OnboardingStep.welcome);
     });
