74dff5fdd1a8eb39aaa4663d2d5ce60f6b6a41e4
Jeremy Norman <jeremy.norman@wearespindle.com>
2023-01-16 10:23:16 +0100

    Using correct avatars for colleague list

--- lib/app/pages/main/widgets/colltact_list/widgets/avatar.dart
+++ lib/app/pages/main/widgets/colltact_list/widgets/avatar.dart
@@ -1,33 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 
 import '../../../../../../data/models/colltact.dart';
 import '../../../../../../domain/colltacts/contact.dart';
+import '../../../../../../domain/user_availability/colleagues/colleague.dart';
+import '../../../../../resources/theme.dart';
 import '../../../../../util/contact.dart';
 import '../../../widgets/avatar.dart';
 import '../util/color.dart';
 
-class ContactAvatar extends StatelessWidget {
-  static const defaultSize = Avatar.defaultSize;
-
-  final Contact contact;
-  final double size;
-
-  const ContactAvatar(
-    this.contact, {
-    Key? key,
-    this.size = defaultSize,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Avatar(
-      name: contact.displayName,
-      backgroundColor: contact.calculateColor(context),
-      image: contact.avatar,
-      size: size,
-    );
-  }
-}
+part 'avatar.freezed.dart';
 
 class ColltactAvatar extends StatelessWidget {
   static const defaultSize = Avatar.defaultSize;
@@ -44,15 +27,160 @@ class ColltactAvatar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return colltact.when(
-      colleague: (colleague) => Avatar(
-        name: colleague.name,
-        backgroundColor: colleague.calculateColor(context),
-        // We don't have an image for colleagues currently, but it will be
-        // supported in the future.
-        image: null,
-        size: size,
-      ),
+      colleague: ColleagueAvatar.new,
       contact: ContactAvatar.new,
     );
   }
+}
+
+class ContactAvatar extends StatelessWidget {
+  final Contact contact;
+  final double size;
+
+  const ContactAvatar(
+    this.contact, {
+    Key? key,
+    this.size = ColltactAvatar.defaultSize,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Avatar(
+      name: contact.displayName,
+      backgroundColor: contact.calculateColor(context),
+      image: contact.avatar,
+      size: size,
+    );
+  }
+}
+
+class ColleagueAvatar extends StatelessWidget {
+  final Colleague colleague;
+  final double size;
+
+  const ColleagueAvatar(
+    this.colleague, {
+    Key? key,
+    this.size = ColltactAvatar.defaultSize,
+  }) : super(key: key);
+
+  _AvatarColor _color(BuildContext context) {
+    final colors = context.brand.theme.colors;
+    final relevantContext = colleague.mostRelevantContext;
+
+    final busy = _AvatarColor(
+      foreground: colors.userAvailabilityBusyAccent,
+      background: colors.userAvailabilityBusy,
+    );
+    final available = _AvatarColor(
+      foreground: colors.userAvailabilityAvailableAccent,
+      background: colors.userAvailabilityAvailable,
+    );
+    final unavailable = _AvatarColor(
+      foreground: colors.userAvailabilityUnavailableAccent,
+      background: colors.userAvailabilityUnavailable,
+    );
+    final unknown = _AvatarColor(
+      foreground: colors.userAvailabilityUnknownAccent,
+      background: colors.userAvailabilityUnknown,
+    );
+
+    if (relevantContext != null) {
+      return relevantContext.when(
+        ringing: () => busy,
+        inCall: () => busy,
+      );
+    }
+
+    return colleague.map(
+      (colleague) {
+        switch (colleague.status) {
+          case ColleagueAvailabilityStatus.available:
+            return available;
+          case ColleagueAvailabilityStatus.doNotDisturb:
+            return unavailable;
+          case ColleagueAvailabilityStatus.busy:
+            return busy;
+          case ColleagueAvailabilityStatus.unknown:
+            return unknown;
+          case ColleagueAvailabilityStatus.offline:
+            return unknown;
+          default:
+            return unknown;
+        }
+      },
+      unconnectedVoipAccount: (_) => unknown,
+    );
+  }
+
+  IconData _icon() {
+    final context = colleague.mostRelevantContext;
+
+    if (context != null) {
+      return context.when(
+        ringing: () => FontAwesomeIcons.phoneVolume,
+        inCall: () => FontAwesomeIcons.phoneVolume,
+      );
+    }
+
+    // We will choose an icon based on the account type, this will only be used
+    // based on their availability status below.
+    final accountTypeIcon = colleague.map(
+      (colleague) {
+        switch (colleague.destination?.type) {
+          case ColleagueDestinationType.app:
+            return FontAwesomeIcons.mobile;
+          default:
+            return FontAwesomeIcons.phoneOffice;
+        }
+      },
+      unconnectedVoipAccount: (_) => FontAwesomeIcons.phoneOffice,
+    );
+
+    return colleague.map(
+      (colleague) {
+        switch (colleague.status) {
+          case ColleagueAvailabilityStatus.available:
+            return FontAwesomeIcons.wifi;
+          case ColleagueAvailabilityStatus.doNotDisturb:
+            return FontAwesomeIcons.bellSlash;
+          case ColleagueAvailabilityStatus.busy:
+            return FontAwesomeIcons.phoneVolume;
+          case ColleagueAvailabilityStatus.unknown:
+            return accountTypeIcon;
+          case ColleagueAvailabilityStatus.offline:
+            return FontAwesomeIcons.wifiSlash;
+          default:
+            return FontAwesomeIcons.phoneOffice;
+        }
+      },
+      unconnectedVoipAccount: (_) => FontAwesomeIcons.phoneOffice,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final color = _color(context);
+
+    return Container(
+      width: size,
+      alignment: Alignment.center,
+      child: AspectRatio(
+        aspectRatio: 1 / 1,
+        child: CircleAvatar(
+          backgroundColor: color.background,
+          foregroundColor: color.foreground,
+          child: FaIcon(_icon(), size: size / 2.5),
+        ),
+      ),
+    );
+  }
+}
+
+@freezed
+class _AvatarColor with _$_AvatarColor {
+  const factory _AvatarColor({
+    required Color foreground,
+    required Color background,
+  }) = __AvatarColor;
 }
