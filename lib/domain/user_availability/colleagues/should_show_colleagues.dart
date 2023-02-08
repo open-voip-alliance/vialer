5fbf92bab5ad82c3113695ab57c269f4d56d586f
Jeremy Norman <jeremy.norman@wearespindle.com>
2023-02-08 18:24:06 +0100

    Resolve "Colleague list does not show up upgrading from previous version"

--- lib/domain/user_availability/colleagues/should_show_colleagues.dart
+++ lib/domain/user_availability/colleagues/should_show_colleagues.dart
@@ -1,3 +1,4 @@
+import '../../../app/util/loggable.dart';
 import '../../feature/feature.dart';
 import '../../feature/has_feature.dart';
 import '../../use_case.dart';
@@ -5,11 +6,25 @@ import '../../user/get_logged_in_user.dart';
 
 /// Whether the app should show colleagues. Takes into account the feature
 /// flag and permissions.
-class ShouldShowColleagues extends UseCase {
+class ShouldShowColleagues extends UseCase with Loggable {
   late final _hasFeature = HasFeature();
   late final _getUser = GetLoggedInUserUseCase();
 
-  bool call() =>
-      _hasFeature(Feature.colleagueList) &&
-      _getUser().permissions.canViewColleagues;
+  bool call() {
+    final hasFeature = _hasFeature(Feature.colleagueList);
+    final hasPermission = _getUser().permissions.canViewColleagues;
+
+    final shouldShow = hasFeature && hasPermission;
+
+    if (!shouldShow) {
+      // Temporary log just while we are trying to resolve an issue with the
+      // colleague list not showing.
+      logger.info(
+        'Not showing colleague list: '
+        'hasFeature=$hasFeature | hasPermission=$hasPermission',
+      );
+    }
+
+    return shouldShow;
+  }
 }
