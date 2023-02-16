2a46dd5ccd117d805d013cf462ccd6dcdc131121
Arjen Vellinga <arjen.vellinga@wearespindle.com>
2023-02-16 12:52:44 +0100

    freezed ClientVoipConfig

--- lib/domain/voipgrid/client_voip_config.dart
+++ lib/domain/voipgrid/client_voip_config.dart
@@ -1,29 +1,23 @@
-import 'package:equatable/equatable.dart';
-import 'package:json_annotation/json_annotation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 
 import '../user/get_brand.dart';
 
+part 'client_voip_config.freezed.dart';
 part 'client_voip_config.g.dart';
 
-@JsonSerializable()
-class ClientVoipConfig extends Equatable {
-  @JsonKey(name: 'MIDDLEWARE', fromJson: _middlewareUrlFromJson)
-  final Uri middlewareUrl;
+@freezed
+class ClientVoipConfig with _$ClientVoipConfig {
+  const ClientVoipConfig._();
 
-  @JsonKey(name: 'SIP_UDP')
-  final Uri unencryptedSipUrl;
-
-  @JsonKey(name: 'SIP_TLS')
-  final Uri encryptedSipUrl;
+  const factory ClientVoipConfig({
+    @JsonKey(name: 'MIDDLEWARE', fromJson: _middlewareUrlFromJson)
+        required Uri middlewareUrl,
+    @JsonKey(name: 'SIP_UDP') required Uri unencryptedSipUrl,
+    @JsonKey(name: 'SIP_TLS') required Uri encryptedSipUrl,
+  }) = _ClientVoipConfig;
 
   static Uri _middlewareUrlFromJson(String json) =>
       Uri.parse(json.startsWith('https://') ? json : 'https://$json');
-
-  const ClientVoipConfig({
-    required this.middlewareUrl,
-    required this.unencryptedSipUrl,
-    required this.encryptedSipUrl,
-  });
 
   /// Fallback config created based on the current brand.
   factory ClientVoipConfig.fallback() {
@@ -39,22 +33,9 @@ class ClientVoipConfig extends Equatable {
   /// Whether this config is equal to [ClientVoipConfig.fallback].
   bool get isFallback => this == ClientVoipConfig.fallback();
 
-  @override
-  List<Object?> get props => [
-        middlewareUrl,
-        unencryptedSipUrl,
-        encryptedSipUrl,
-      ];
-
-  @override
-  String toString() => '$runtimeType('
-      'middlewareUrl: $middlewareUrl, '
-      'unencryptedSipUrl: $unencryptedSipUrl, '
-      'encryptedSipUrl: $encryptedSipUrl)';
-
-  static ClientVoipConfig fromJson(Map<String, dynamic> json) =>
+  factory ClientVoipConfig.fromJson(Map<String, dynamic> json) =>
       _$ClientVoipConfigFromJson(json);
 
-  static Map<String, dynamic> toJson(ClientVoipConfig value) =>
-      _$ClientVoipConfigToJson(value);
+  static Map<String, dynamic> serializeToJson(ClientVoipConfig config) =>
+      config.toJson();
 }
