2ed4d9f4d880914b51bebea1d6df2e4667ba8c76
Arjen Vellinga <arjen.vellinga@wearespindle.com>
2023-02-16 12:40:45 +0100

    freezed VoipgridPhoneAccount

--- lib/domain/voipgrid/voipgrid_phone_account.dart
+++ lib/domain/voipgrid/voipgrid_phone_account.dart
@@ -1,33 +1,23 @@
-import 'package:json_annotation/json_annotation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 
 import '../../app/util/json_converter.dart';
 import '../calling/voip/destination.dart';
 import 'voipgrid_destination.dart';
 
+part 'voipgrid_phone_account.freezed.dart';
 part 'voipgrid_phone_account.g.dart';
 
+@freezed
 @JsonSerializable(fieldRename: FieldRename.snake)
-class VoipgridPhoneAccount extends VoipgridDestination {
-  @override
-  @JsonIdConverter()
-  final int? id;
-
-  final int accountId;
-
-  final int internalNumber;
-
-  @override
-  final String description;
-
-  final String password;
-
-  const VoipgridPhoneAccount({
-    required this.id,
-    required this.accountId,
-    required this.internalNumber,
-    required this.description,
-    required this.password,
-  });
+class VoipgridPhoneAccount extends VoipgridDestination
+    with _$VoipgridPhoneAccount {
+  const factory VoipgridPhoneAccount({
+    @override @JsonIdConverter() int? id,
+    required int accountId,
+    required int internalNumber,
+    @override required String description,
+    required String password,
+  }) = _VoipgridPhoneAccount;
 
   factory VoipgridPhoneAccount.fromJson(Map<String, dynamic> json) =>
       _$VoipgridPhoneAccountFromJson(json);
