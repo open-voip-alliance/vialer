4f307c78c07ec7bf480b0fa81a5b52386b1fe48b
Jeremy Norman <jeremy.norman@wearespindle.com>
2023-02-25 09:29:44 +0100

    Added RetryTask to automatically retry arbitrary code, added test for it,...

---
+++ lib/app/util/automatic_retry.dart
@@ -0,0 +1,148 @@
+import 'dart:async';
+
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+import 'loggable.dart';
+
+part 'automatic_retry.freezed.dart';
+
+class AutomaticRetry with Loggable {
+  final _timers = <Timer>[];
+
+  /// The [schedule] provided is relative to the first time it is run, not the
+  /// previous retry attempt.
+  final List<Duration> schedule;
+
+  /// If a name is provided, logging will be performed when the task fails.
+  final String? name;
+
+  AutomaticRetry({required this.schedule, this.name});
+
+  factory AutomaticRetry.http(String name) => AutomaticRetry(
+        schedule: RetrySchedule.http,
+        name: name,
+      );
+
+  /// Will automatically retry the provided task, according to the given
+  /// schedule.
+  ///
+  /// If every run in the schedule fails, [AutomaticRetryMaximumAttemptsReached]
+  /// will be thrown.
+  ///
+  /// Set [runImmediately] to [false] for the first attempt to occur at the
+  /// first entry in the schedule, rather than immediately.
+  Future<T> run<T>(
+    Future<AutomaticRetryTaskOutput<T>> Function() task, {
+    bool runImmediately = true,
+  }) async {
+    _timers.cancelAll();
+
+    if (!runImmediately && schedule.isEmpty) {
+      throw ArgumentError(
+        'You must either provide a schedule, or enable run immediately, '
+        'otherwise nothing will ever execute.',
+      );
+    }
+
+    if (runImmediately) {
+      final output = await task();
+
+      if (output.result == AutomaticRetryTaskResult.success) {
+        return output.data as T;
+      }
+
+      if (schedule.isEmpty) {
+        _logTaskAsFailed(runImmediately: runImmediately);
+        throw AutomaticRetryMaximumAttemptsReached();
+      }
+    }
+
+    final completer = Completer<T>();
+
+    _scheduleTimers<T>(task, completer, runImmediately: runImmediately);
+
+    return completer.future;
+  }
+
+  void _scheduleTimers<T>(
+    Future<dynamic> Function() task,
+    Completer<dynamic> completer, {
+    required bool runImmediately,
+  }) {
+    for (var duration in schedule) {
+      final timer = Timer(duration, () async {
+        final output = await task();
+        if (output.result == AutomaticRetryTaskResult.success) {
+          _timers.cancelAll();
+          return completer.complete(output.data as T);
+        } else {
+          if (!_timers.hasAnyActive) {
+            completer.completeError(AutomaticRetryMaximumAttemptsReached());
+            _logTaskAsFailed(runImmediately: runImmediately);
+            return;
+          }
+        }
+      });
+
+      _timers.add(timer);
+    }
+  }
+
+  void _logTaskAsFailed({required bool runImmediately}) {
+    if (name != null) {
+      final attempts = runImmediately ? schedule.length + 1 : schedule.length;
+
+      logger.warning(
+        'AutomaticRetryTask [$name] has failed after [$attempts] attempts.',
+      );
+    }
+  }
+}
+
+extension on List<Timer> {
+  void cancelAll() {
+    forEach((timer) => timer.cancel());
+    clear();
+  }
+
+  bool get hasAnyActive => where((timer) => timer.isActive).isNotEmpty;
+}
+
+/// Indicates that the retry task has reached the maximum number of attempts
+/// and will stop trying any further.
+class AutomaticRetryMaximumAttemptsReached implements Exception {}
+
+class RetrySchedule {
+  /// This is a sensible default schedule for http requests, for other uses
+  /// you likely want to use a custom schedule.
+  static const http = [
+    Duration(seconds: 1),
+    Duration(seconds: 3),
+    Duration(seconds: 5),
+  ];
+}
+
+/// Contains the result of an individual task, the task must provide both the
+/// [AutomaticRetryTaskResult] and the data that it should output.
+@freezed
+class AutomaticRetryTaskOutput<T> with _$AutomaticRetryTaskOutput {
+  const factory AutomaticRetryTaskOutput({
+    required T data,
+    required AutomaticRetryTaskResult result,
+  }) = _AutomaticRetryTaskOutput;
+
+  factory AutomaticRetryTaskOutput.success(T data) => AutomaticRetryTaskOutput(
+        data: data,
+        result: AutomaticRetryTaskResult.success,
+      );
+
+  factory AutomaticRetryTaskOutput.fail(T data) => AutomaticRetryTaskOutput(
+        data: data,
+        result: AutomaticRetryTaskResult.fail,
+      );
+}
+
+enum AutomaticRetryTaskResult {
+  success,
+  fail,
+}
---
+++ test/app/util/automatic_retry_test.dart
@@ -0,0 +1,155 @@
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:test/test.dart';
+import 'package:vialer/app/util/automatic_retry.dart';
+
+@GenerateNiceMocks([MockSpec<DummyClass>()])
+import 'automatic_retry_test.mocks.dart';
+
+void main() {
+  test('A task does not retry if it succeeds', () async {
+    final automaticRetry = AutomaticRetry(
+      schedule: const [Duration(milliseconds: 50)],
+    );
+    final mock = MockDummyClass()..thenReturnSuccess();
+    await automaticRetry.run(() async => mock.getResult());
+    verify(mock.getResult()).called(1);
+  });
+
+  test('A task ends even if there is no schedule', () async {
+    final automaticRetry = AutomaticRetry(schedule: []);
+    final mock = MockDummyClass()..thenReturnFail();
+    await _expectMaximumAttemptsReached(automaticRetry, mock);
+  });
+
+  test('Argument error is thrown if no schedule and not running immediately',
+      () async {
+    final automaticRetry = AutomaticRetry(schedule: []);
+
+    await _expectArgumentError(
+      () => automaticRetry.run(
+        () async => const AutomaticRetryTaskOutput(
+          data: '',
+          result: AutomaticRetryTaskResult.fail,
+        ),
+        runImmediately: false,
+      ),
+    );
+  });
+
+  test('A task will retry according to schedule if it fails', () async {
+    final automaticRetry = AutomaticRetry(
+      schedule: const [
+        Duration(milliseconds: 10),
+        Duration(milliseconds: 20),
+        Duration(milliseconds: 30),
+      ],
+    );
+    final mock = MockDummyClass()..thenReturnFail();
+
+    await _expectMaximumAttemptsReached(automaticRetry, mock);
+    verify(mock.getResult()).called(4);
+  });
+
+  test('A task will stop retrying if it completes successfully', () async {
+    final automaticRetry = AutomaticRetry(
+      schedule: const [
+        // We want to make sure the schedule is full as only the first two
+        // in the schedule should be called.
+        Duration(milliseconds: 10),
+        Duration(milliseconds: 20),
+        Duration(milliseconds: 30),
+        Duration(milliseconds: 40),
+        Duration(milliseconds: 50),
+        Duration(milliseconds: 60),
+      ],
+    );
+    final mock = MockDummyClass();
+
+    // There is no in-built mechanism in Mockito to provide different responses
+    // depending on how many times it is called, so we will have to fake it
+    // with this local variable.
+    var runs = 0;
+    when(mock.getResult()).thenAnswer((_) {
+      if (runs < 2) {
+        runs++;
+        return const AutomaticRetryTaskOutput(
+          data: '',
+          result: AutomaticRetryTaskResult.fail,
+        );
+      }
+
+      return const AutomaticRetryTaskOutput(
+        data: 'A valid value',
+        result: AutomaticRetryTaskResult.success,
+      );
+    });
+
+    expect(
+      await automaticRetry.run(() async => mock.getResult()),
+      'A valid value',
+    );
+    verify(mock.getResult()).called(3);
+  });
+
+  test('Running a task again will cancel all existing retries', () async {
+    final automaticRetry = AutomaticRetry(schedule: const [
+      Duration(milliseconds: 10),
+    ]);
+    final failingMock = MockDummyClass()..thenReturnFail();
+
+    automaticRetry.run(
+      () async => failingMock.getResult(),
+      runImmediately: false,
+    );
+
+    final successMock = MockDummyClass()..thenReturnSuccess();
+
+    await automaticRetry.run(() async => successMock.getResult());
+
+    // Adding a delay that must be longer than the duration defined at the top
+    // of the test to make sure it is never called.
+    await Future.delayed(const Duration(milliseconds: 100));
+
+    // The first mock should never get called as [runImmediately] is false.
+    verifyNever(failingMock.getResult());
+    verify(successMock.getResult()).called(1);
+  });
+}
+
+extension on MockDummyClass {
+  void thenReturnSuccess({String value = ''}) => when(getResult()).thenReturn(
+        AutomaticRetryTaskOutput(
+          data: value,
+          result: AutomaticRetryTaskResult.success,
+        ),
+      );
+
+  void thenReturnFail({String value = ''}) => when(getResult()).thenReturn(
+        AutomaticRetryTaskOutput(
+          data: value,
+          result: AutomaticRetryTaskResult.fail,
+        ),
+      );
+}
+
+Future<void> _expectArgumentError(Future<String> Function() callback) =>
+    expectLater(
+      callback,
+      throwsA(isA<ArgumentError>()),
+    );
+
+Future<void> _expectMaximumAttemptsReached(
+  AutomaticRetry automaticRetry,
+  MockDummyClass mock,
+) async =>
+    expectLater(
+      () => automaticRetry.run(() async => mock.getResult()),
+      throwsA(isA<AutomaticRetryMaximumAttemptsReached>()),
+    );
+
+// A dummy class just used so we can mock it and verify interactions, it needs
+// to be public so it can be mocked.
+class DummyClass {
+  AutomaticRetryTaskOutput<String> getResult() => throw Error();
+}
