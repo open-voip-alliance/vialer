c71362e583742f8384e2debd8669861c5da48175
Wilko Manger <wilko.manger@wearespindle.com>
2023-03-02 14:36:03 +0100

    Apply 1 suggestion(s) to 1 file(s)

--- lib/domain/user/refresh_user.dart
+++ lib/domain/user/refresh_user.dart
@@ -64,7 +64,7 @@ class RefreshUser extends UseCase with Loggable {
     LoginCredentials? credentials,
     List<UserRefreshTask> tasksToRun,
   ) async {
-    executeUserRefreshTasks() async {
+    Future<void> executeUserRefreshTasks() async {
       final storedUser = _storageRepository.user;
       final latestUser = await _getUserFromCredentials(credentials);
 
