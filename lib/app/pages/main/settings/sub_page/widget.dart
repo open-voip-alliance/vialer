3628b33cb68ac44e85847bc9f699af63a79b5206
Jeremy Norman <jeremy.norman@wearespindle.com>
2023-03-09 22:44:43 +0100

    New settings screen

--- lib/app/pages/main/settings/sub_page/widget.dart
+++ lib/app/pages/main/settings/sub_page/widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../../resources/theme.dart';
 import '../../widgets/full_screen_page.dart';
 import '../cubit.dart';
 
@@ -31,6 +32,36 @@ class SettingsSubPage extends StatelessWidget {
               children: children(state),
             );
           },
+        ),
+      ),
+    );
+  }
+}
+
+class SettingsSubPageHelp extends StatelessWidget {
+  final String text;
+
+  const SettingsSubPageHelp(this.text);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 16,
+        vertical: 10,
+      ),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          color: context.brand.theme.colors.grey3,
+        ),
+        padding: const EdgeInsets.symmetric(
+          horizontal: 12,
+          vertical: 16,
+        ),
+        child: Text(
+          text,
+          style: const TextStyle(fontSize: 15),
         ),
       ),
     );
