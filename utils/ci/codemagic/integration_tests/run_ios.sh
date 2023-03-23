a62f2ba88673375798bd0e674ec6991498e5fdd3
Jeremy Norman <jeremy.norman@wearespindle.com>
2023-03-23 22:20:27 +0100

    Running tests on iOS

--- utils/ci/codemagic/integration_tests/run_ios.sh
+++ utils/ci/codemagic/integration_tests/run_ios.sh
@@ -5,7 +5,7 @@ brew install applesimutils
 xcrun simctl shutdown all
 TEST_DEVICE=$(xcrun simctl create test-device com.apple.CoreSimulator.SimDeviceType.iPhone-14-Pro com.apple.CoreSimulator.SimRuntime.iOS-16-2)
 xcrun simctl boot $TEST_DEVICE
-flutter build ios --debug --target=lib/app/main.dart
+flutter build ipa --debug --target=lib/app/main.dart
 for filename in integration_test/tests/**/*.dart; do
   flutter -d $TEST_DEVICE install --debug --verbose
   applesimutils --booted --bundle com.voipgrid.vialer --setPermissions contacts=YES,microphone=YES,notifications=YES
