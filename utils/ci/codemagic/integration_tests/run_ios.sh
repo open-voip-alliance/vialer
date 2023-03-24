d1feaa14f31eab77e2c03b18c1c16ddc134e7bd7
Jeremy Norman <jeremy.norman@wearespindle.com>
2023-03-24 09:21:07 +0100

    Running tests on iOS

--- utils/ci/codemagic/integration_tests/run_ios.sh
+++ utils/ci/codemagic/integration_tests/run_ios.sh
@@ -6,7 +6,7 @@ xcrun simctl shutdown all
 TEST_DEVICE=$(xcrun simctl create test-device com.apple.CoreSimulator.SimDeviceType.iPhone-14-Pro com.apple.CoreSimulator.SimRuntime.iOS-16-2)
 xcrun simctl boot $TEST_DEVICE
 for filename in integration_test/tests/**/*.dart; do
-  flutter -d $TEST_DEVICE install --verbose
+  flutter install --verbose --use-application-binary-path=build/ios/ipa/Vialer.ipa -d $TEST_DEVICE
   applesimutils --booted --bundle com.voipgrid.vialer --setPermissions contacts=YES,microphone=YES,notifications=YES
-  flutter drive -t "$filename" --driver test_driver/integration_test.dart
+  flutter drive -t "$filename" --driver test_driver/integration_test.dart -d $TEST_DEVICE
 done
