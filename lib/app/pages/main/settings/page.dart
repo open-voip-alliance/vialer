43072213c590cf8c90ea6f6cb7896b2f264f4501
Wilko Manger <wilko.manger@wearespindle.com>
2023-04-11 14:34:25 +0200

    Keep bottom bar while navigating Settings

--- lib/app/pages/main/settings/page.dart
+++ lib/app/pages/main/settings/page.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import '../../../resources/localizations.dart';
+import '../widgets/nested_navigator.dart';
 import 'cubit.dart';
 import 'header/widget.dart';
 import 'sub_page/app_preferences.dart';
@@ -28,90 +29,100 @@ class _SettingsPageState extends State<SettingsPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.only(
-            top: 16,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              BlocProvider<SettingsCubit>(
-                create: (_) => SettingsCubit(),
-                child: BlocConsumer<SettingsCubit, SettingsState>(
-                  listener: _onStateChanged,
-                  builder: (context, state) {
-                    final user = state.user;
-                    final showDnd = state.showDnd;
-                    final userNumber = state.userNumber;
-                    final destinations = state.availableDestinations;
-                    final cubit = context.watch<SettingsCubit>();
+    return NestedNavigator(
+      routes: {
+        'root': (context, _) {
+          return Scaffold(
+            resizeToAvoidBottomInset: false,
+            body: SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.only(
+                  top: 16,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    BlocProvider<SettingsCubit>(
+                      create: (_) => SettingsCubit(),
+                      child: BlocConsumer<SettingsCubit, SettingsState>(
+                        listener: _onStateChanged,
+                        builder: (context, state) {
+                          final user = state.user;
+                          final showDnd = state.showDnd;
+                          final userNumber = state.userNumber;
+                          final destinations = state.availableDestinations;
+                          final cubit = context.watch<SettingsCubit>();
 
-                    return Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 20),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Expanded(
-                              child: SingleChildScrollView(
-                                child: Column(
-                                  children: [
-                                    Header(user: state.user),
-                                    if (showDnd)
-                                      DndTile(
-                                        user,
-                                        enabled: !state.isApplyingChanges,
+                          return Expanded(
+                            child: Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 20),
+                              child: Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Expanded(
+                                    child: SingleChildScrollView(
+                                      child: Column(
+                                        children: [
+                                          Header(user: state.user),
+                                          if (showDnd)
+                                            DndTile(
+                                              user,
+                                              enabled: !state.isApplyingChanges,
+                                            ),
+                                          AvailabilityTile(
+                                            user: user,
+                                            userNumber: userNumber,
+                                            destinations: destinations,
+                                            enabled: !state.isApplyingChanges,
+                                          ),
+                                          SubPageLinkTile(
+                                            title: context.msg.main.settings
+                                                .subPage.appPreferences.title,
+                                            icon: FontAwesomeIcons
+                                                .solidMobileNotch,
+                                            cubit: cubit,
+                                            pageBuilder: (_) =>
+                                                const AppPreferencesSubPage(),
+                                          ),
+                                          SubPageLinkTile(
+                                            title: context
+                                                .msg.main.settings.subPage.user
+                                                .title(user.fullName),
+                                            icon: FontAwesomeIcons.circleUser,
+                                            cubit: cubit,
+                                            pageBuilder: (_) =>
+                                                const UserSubPage(),
+                                          ),
+                                          if (user.canViewClientSubPage)
+                                            SubPageLinkTile(
+                                              title: context.msg.main.settings
+                                                  .subPage.client
+                                                  .title(user.client.name),
+                                              icon: FontAwesomeIcons.building,
+                                              cubit: cubit,
+                                              pageBuilder: (_) =>
+                                                  const ClientSubPage(),
+                                            ),
+                                        ],
                                       ),
-                                    AvailabilityTile(
-                                      user: user,
-                                      userNumber: userNumber,
-                                      destinations: destinations,
-                                      enabled: !state.isApplyingChanges,
                                     ),
-                                    SubPageLinkTile(
-                                      title: context.msg.main.settings.subPage
-                                          .appPreferences.title,
-                                      icon: FontAwesomeIcons.solidMobileNotch,
-                                      cubit: cubit,
-                                      pageBuilder: (_) =>
-                                          const AppPreferencesSubPage(),
-                                    ),
-                                    SubPageLinkTile(
-                                      title: context
-                                          .msg.main.settings.subPage.user
-                                          .title(user.fullName),
-                                      icon: FontAwesomeIcons.circleUser,
-                                      cubit: cubit,
-                                      pageBuilder: (_) => const UserSubPage(),
-                                    ),
-                                    if (user.canViewClientSubPage)
-                                      SubPageLinkTile(
-                                        title: context
-                                            .msg.main.settings.subPage.client
-                                            .title(user.client.name),
-                                        icon: FontAwesomeIcons.building,
-                                        cubit: cubit,
-                                        pageBuilder: (_) =>
-                                            const ClientSubPage(),
-                                      ),
-                                  ],
-                                ),
+                                  ),
+                                ],
                               ),
                             ),
-                          ],
-                        ),
+                          );
+                        },
                       ),
-                    );
-                  },
+                    ),
+                  ],
                 ),
               ),
-            ],
-          ),
-        ),
-      ),
+            ),
+          );
+        },
+      },
     );
   }
 }
