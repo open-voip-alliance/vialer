02102b70df76b8a247e3f753eef501fe06376d9c
Jeremy Norman <jeremy.norman@wearespindle.com>
2023-04-18 14:29:49 +0200

    Changing the remaining seconds

--- lib/app/pages/main/settings/widgets/rate_limited_snackbar_label.dart
+++ lib/app/pages/main/settings/widgets/rate_limited_snackbar_label.dart
@@ -6,8 +6,7 @@ import '../../../../resources/theme.dart';
 class RateLimitedSnackbarLabel extends StatefulWidget {
   final DateTime expiresAt;
 
-  const RateLimitedSnackbarLabel({Key? key, required this.expiresAt})
-      : super(key: key);
+  const RateLimitedSnackbarLabel({required this.expiresAt});
 
   @override
   _RateLimitedSnackbarLabelState createState() =>
@@ -16,24 +15,33 @@ class RateLimitedSnackbarLabel extends StatefulWidget {
 
 class _RateLimitedSnackbarLabelState extends State<RateLimitedSnackbarLabel> {
   late Timer _timer;
+  late int remainingSeconds;
 
   @override
   void initState() {
     super.initState();
-    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
-      setState(() {});
-    });
+
+    updateRemainingSeconds() {
+      setState(() {
+        final remaining = widget.expiresAt.difference(DateTime.now()).inSeconds;
+        remainingSeconds = remaining > 0 ? remaining : 0;
+      });
+    }
+
+    updateRemainingSeconds();
+
+    _timer = Timer.periodic(
+      const Duration(seconds: 1),
+      (_) => updateRemainingSeconds(),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    final remainingSeconds =
-        widget.expiresAt.difference(DateTime.now()).inSeconds;
-
     return Text(
       context.msg.rateLimiting.snackbar.message(
         context.brand.appName,
-        remainingSeconds > 0 ? remainingSeconds.toString() : '0',
+        remainingSeconds.toString(),
       ),
     );
   }
