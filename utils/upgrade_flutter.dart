6f27ae8b3ebe8673f2662aaa85cecad7824d5fdd
Jeremy Norman <jeremy.norman@wearespindle.com>
2023-04-18 12:41:50 +0200

    Updated based on review changes

--- utils/upgrade_flutter.dart
+++ utils/upgrade_flutter.dart
@@ -1,5 +1,22 @@
 import 'dart:io';
 
+/// Upgrades Flutter to a specified version, updates the 'codemagic.yaml' and
+/// 'pubspec.yaml' files with the new Flutter version, creates a new git branch
+/// with the specified name, commits the changes, and pushes the branch to the
+/// remote repository.
+///
+/// The script requires two arguments: the first argument is the version number
+/// of the Flutter SDK to upgrade to, and the second argument is the ticket ID
+/// from Gitlab that the changes are related to.
+///
+/// Before running this script, make sure you have committed and pushed any
+/// changes to the remote repository. The script will switch to the 'develop'
+/// branch, pull the latest changes from the remote repository, create a new
+/// branch with the specified name, and push the changes to the remote
+/// repository on this new branch.
+///
+/// Only the 'pubspec.yaml', 'pubspec.lock', and 'codemagic.yaml' files will be
+/// added to the git commit.
 Future<void> main(List<String> arguments) async {
   if (arguments.length != 2) {
     throw Exception(
@@ -16,32 +33,32 @@ Future<void> main(List<String> arguments) async {
       'Please commit or discard them before running this script.',
     );
   }
-print('as');
-  // await _runProcess('flutter', ['upgrade']);
-  //
-  // await _runProcess('git', ['checkout', 'develop']);
-  // await _runProcess('git', ['pull', 'origin', 'develop']);
-  //
-  // final newBranchName = '$ticketId-upgrade-flutter-$versionNumber';
-  // await _runProcess('git', ['checkout', '-b', newBranchName]);
-  //
-  // await _updateYaml('codemagic.yaml', versionNumber);
-  // await _updateYaml('pubspec.yaml', versionNumber);
-  //
-  // await _runProcess('flutter', ['pub', 'get']);
-  //
-  // await _runProcess('git', [
-  //   'add',
-  //   'pubspec.lock',
-  //   'pubspec.yaml',
-  //   'codemagic.yaml',
-  // ]);
-  //
-  // await _runProcess(
-  //   'git',
-  //   ['commit', '-m', 'Upgrade Flutter to version $versionNumber'],
-  // );
-  // await _runProcess('git', ['push', 'origin', newBranchName]);
+
+  await _runProcess('flutter', ['upgrade']);
+
+  await _runProcess('git', ['checkout', 'develop']);
+  await _runProcess('git', ['pull', 'origin', 'develop']);
+
+  final newBranchName = '$ticketId-upgrade-flutter-$versionNumber';
+  await _runProcess('git', ['checkout', '-b', newBranchName]);
+
+  await _updateYaml('codemagic.yaml', versionNumber);
+  await _updateYaml('pubspec.yaml', versionNumber);
+
+  await _runProcess('flutter', ['pub', 'get']);
+
+  await _runProcess('git', [
+    'add',
+    'pubspec.lock',
+    'pubspec.yaml',
+    'codemagic.yaml',
+  ]);
+
+  await _runProcess(
+    'git',
+    ['commit', '-m', 'Upgrade Flutter to version $versionNumber'],
+  );
+  await _runProcess('git', ['push', 'origin', newBranchName]);
 }
 
 Future<void> _updateYaml(String filePath, String value) async {
