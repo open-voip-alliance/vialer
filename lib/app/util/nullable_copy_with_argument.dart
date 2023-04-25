524a355ca1f87fdbe72bb024b88278a45518afde
Wilko Manger <wilko.manger@wearespindle.com>
2023-04-25 12:37:35 +0200

    Apply 2 suggestion(s) to 1 file(s)

--- lib/app/util/nullable_copy_with_argument.dart
+++ lib/app/util/nullable_copy_with_argument.dart
@@ -1,18 +1,18 @@
-/// Describing a parameter that can either be [T!]|null|undefined.
+/// Describing a parameter that can either be [T]`|null|<unchanged>`.
 ///
-/// Passing these to a a [copyWith] method allows for setting a value to [null]
-/// while also being able to provide a value that represents [undefined], a
+/// Passing these to a a `copyWith` method allows for setting a value to `null`
+/// while also being able to provide a value that represents `<unchanged>`, a
 /// property of the class that we don't want to modify.
 ///
 /// If you provide a callback: the result of that callback will be treated
 /// as a property to set on the entity that is being updated, even if this
-/// is a [null] value.
+/// is a `null` value.
 ///
-/// If you provide [null] rather than a callback: the argument will be ignored±
+/// If you provide `null` rather than a callback: the argument will be ignored±
 /// and not modified on the entity.
 ///
-/// Without this it would not be possible to set values to [null] in
-/// a [copyWith].
+/// Without this it would not be possible to set values to `null` in
+/// a `copyWith`.
 typedef NullableCopyWithArgument<T> = T? Function()?;
 
 extension Value<T> on NullableCopyWithArgument<T> {
