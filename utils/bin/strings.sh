9b76241082d871a8f3f045dea77035170e3dafce
Jeremy Norman <jeremy.norman@wearespindle.com>
2023-05-23 10:00:17 +0200

    Automatically importing phrase

--- utils/bin/strings.sh
+++ utils/bin/strings.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-#if [[ $# -eq 0 && -z $PHRASE_ACCESS_TOKEN ]]; then
+if [[ $# -eq 0 && -z $PHRASE_ACCESS_TOKEN ]]; then
   echo "Error: PHRASE_ACCESS_TOKEN not provided."
   echo "Please set the PHRASE_ACCESS_TOKEN environment variable or provide it as a command line argument."
   echo "This token can be found in Keyhub under the name \"Phrase Strings Access Token\""
   echo "Alternatively, follow this link: https://keyhub.wearespindle.com/console/vaults/record-60000681-5d8d-4125-90f2-d3c66e4a4bf3-Phrase_Strings_Access_Token"
   echo "To set the environment variable, use: export PHRASE_ACCESS_TOKEN=your_token"
   return 1
-#fi
+fi
 
 if [[ $# -eq 1 ]]; then
   PHRASE_ACCESS_TOKEN=$1
