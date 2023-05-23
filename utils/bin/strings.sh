686bb6a236b0ad86fea83aad879c2ec47f9021ff
Jeremy Norman <jeremy.norman@wearespindle.com>
2023-05-23 11:14:12 +0200

    Added code generation step

--- utils/bin/strings.sh
+++ utils/bin/strings.sh
@@ -5,6 +5,7 @@ if [[ $# -eq 0 && -z $PHRASE_ACCESS_TOKEN ]]; then
   echo "Please set the PHRASE_ACCESS_TOKEN environment variable or provide it as a command line argument."
   echo "This should be a local environment variable, not one added to the .env file."
   echo "To set the environment variable, use: export PHRASE_ACCESS_TOKEN=your_token"
+  echo "Or update your ~/.bash_profile or ~/.zshrc to store it permanently."
   echo "The access token can be found in Keyhub under the name \"Phrase Strings Access Token\""
   echo "Alternatively, follow this link: https://keyhub.wearespindle.com/console/vaults/record-60000681-5d8d-4125-90f2-d3c66e4a4bf3-Phrase_Strings_Access_Token"
   return 1
