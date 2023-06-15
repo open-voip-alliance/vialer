79eb13d9b34e531e906dbd00ef37c8d664c063e8
Jeremy Norman <jeremy.norman@wearespindle.com>
2023-06-15 14:27:17 +0200

    Created a widget for eventlisteners so consumers don't need to handle them specifically

--- lib/domain/event/event_bus.dart
+++ lib/domain/event/event_bus.dart
@@ -9,6 +9,8 @@ extension Broadcasting on EventBus {
   void broadcast(EventBusEvent event) => add(event);
 }
 
+// When using within a widget, you should always use the EventBusListener widget
+// while will handle disposing of the listener properly.
 extension Observing on EventBusObserver {
   StreamSubscription<T> on<T>(
     void Function(T event)? onData, {
