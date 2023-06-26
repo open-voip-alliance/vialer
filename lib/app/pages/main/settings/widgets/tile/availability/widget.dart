9f8d39a2c400a059464a7f97c3341000b2b3426a
Jeremy Norman <jeremy.norman@wearespindle.com>
2023-06-26 09:29:06 +0200

    Using user-based do-not-disturb

--- lib/app/pages/main/settings/widgets/tile/availability/widget.dart
+++ lib/app/pages/main/settings/widgets/tile/availability/widget.dart
@@ -1,103 +1,25 @@
-import 'dart:async';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:vialer/app/pages/main/settings/widgets/tile/availability/ringing_device/widget.dart';
-import 'package:vialer/domain/feature/feature.dart';
-import 'package:vialer/domain/feature/has_feature.dart';
-import 'package:vialer/domain/user/get_logged_in_user.dart';
 
-import '../../../../../../../domain/calling/voip/destination.dart';
 import '../../../../../../../domain/user/settings/call_setting.dart';
-import '../../../../../../../domain/user/settings/settings.dart';
-import '../../../../../../../domain/user/user.dart';
-import '../../../../../../../domain/user_availability/colleagues/colleague.dart';
+import '../../../../widgets/user_availability_status_builder/cubit.dart';
 import '../../../../widgets/user_availability_status_builder/widget.dart';
 import '../../../cubit.dart';
 import '../value.dart';
 import '../widget.dart';
 import 'availability_status/widget.dart';
 
-class AvailabilitySwitcher extends StatefulWidget {
+class AvailabilitySwitcher extends StatelessWidget {
   const AvailabilitySwitcher({super.key});
-
-  @override
-  State<AvailabilitySwitcher> createState() => _AvailabilitySwitcherState();
-}
-
-class _AvailabilitySwitcherState extends State<AvailabilitySwitcher> {
-  /// We want the UI to be optimistic, rather than waiting for a server response
-  /// before changing the status. We set this temporarily while making changes
-  /// to immediately update the UI with the user's choice.
-  ColleagueAvailabilityStatus? _statusOverride;
-
-  Future<void> _onAvailabilityStatusChange(
-    List<Destination> destinations,
-    BuildContext context,
-    ColleagueAvailabilityStatus requestedStatus,
-  ) async {
-    setState(() {
-      _statusOverride = requestedStatus;
-    });
-
-    final user = GetLoggedInUserUseCase()();
-
-    await defaultOnSettingsChanged(
-      context,
-      _determineSettingsToModify(user, destinations, context, requestedStatus),
-    );
-
-    if (!HasFeature()(Feature.userBasedDnd)) {
-      // Hacky solution to making sure availability doesn't sometimes flick back
-      // to available. Should be removed with user-based dnd.
-      // ignore: inference_failure_on_instance_creation
-      await Future.delayed(const Duration(seconds: 1));
-      await defaultOnSettingChanged(
-        context,
-        CallSetting.dnd,
-        requestedStatus == ColleagueAvailabilityStatus.doNotDisturb,
-      );
-    }
-
-    _statusOverride = null;
-  }
-
-  Map<SettingKey, Object> _determineSettingsToModify(
-    User user,
-    List<Destination> destinations,
-    BuildContext context,
-    ColleagueAvailabilityStatus requestedStatus,
-  ) {
-    final destination =
-        destinations.findHighestPriorityDestinationFor(user: user);
-
-    return switch (requestedStatus) {
-      ColleagueAvailabilityStatus.available => {
-          CallSetting.dnd: false,
-          if (destination != null) CallSetting.destination: destination,
-        },
-      ColleagueAvailabilityStatus.doNotDisturb => {
-          CallSetting.dnd: true,
-          if (destination != null) CallSetting.destination: destination,
-        },
-      ColleagueAvailabilityStatus.offline => {
-          CallSetting.dnd: false,
-          CallSetting.destination: const Destination.notAvailable(),
-        },
-      _ => throw ArgumentError(
-          'Only [available], [doNotDisturb], [offline] '
-          'are valid options for setting user status.',
-        ),
-    };
-  }
 
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<SettingsCubit, SettingsState>(
-      builder: (context, state) {
+      builder: (_, state) {
         return UserAvailabilityStatusBuilder(
           builder: (context, status) {
-            final userStatus = _statusOverride ?? status;
             return SettingTile(
               padding: EdgeInsets.zero,
               mergeSemantics: false,
@@ -107,15 +29,15 @@ class _AvailabilitySwitcherState extends State<AvailabilitySwitcher> {
                   Padding(
                     padding: const EdgeInsets.only(bottom: 16),
                     child: AvailabilityStatusPicker(
-                      onStatusChanged: (status) async =>
-                          _onAvailabilityStatusChange(
-                        state.availableDestinations,
-                        context,
-                        status,
-                      ),
+                      onStatusChanged: (status) async => context
+                          .read<UserAvailabilityStatusCubit>()
+                          .changeAvailabilityStatus(
+                            status,
+                            state.availableDestinations,
+                          ),
                       user: state.user,
                       enabled: state.shouldAllowRemoteSettings,
-                      userAvailabilityStatus: userStatus,
+                      userAvailabilityStatus: status,
                     ),
                   ),
                   if (state.availableDestinations.length >= 2)
@@ -129,7 +51,7 @@ class _AvailabilitySwitcherState extends State<AvailabilitySwitcher> {
                         destination,
                       ),
                       enabled: state.shouldAllowRemoteSettings,
-                      userAvailabilityStatus: userStatus,
+                      userAvailabilityStatus: status,
                     ),
                 ],
               ),
--- lib/app/pages/main/widgets/user_availability_status_builder/cubit.dart
+++ lib/app/pages/main/widgets/user_availability_status_builder/cubit.dart
@@ -4,6 +4,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:vialer/domain/feature/feature.dart';
 import 'package:vialer/domain/feature/has_feature.dart';
 import 'package:vialer/domain/user/events/logged_in_user_dnd_status_changed.dart';
+import 'package:vialer/domain/user/settings/change_settings.dart';
 import 'package:vialer/domain/user_availability/colleagues/availbility_update.dart';
 import 'package:vialer/domain/user_availability/colleagues/colleague.dart';
 import 'package:vialer/domain/user_availability/colleagues/colleagues_repository.dart';
@@ -13,8 +14,10 @@ import '../../../../../domain/calling/voip/destination.dart';
 import '../../../../../domain/event/event_bus.dart';
 import '../../../../../domain/user/events/logged_in_user_availability_changed.dart';
 import '../../../../../domain/user/events/logged_in_user_was_refreshed.dart';
+import '../../../../../domain/user/get_logged_in_user.dart';
 import '../../../../../domain/user/get_stored_user.dart';
 import '../../../../../domain/user/settings/call_setting.dart';
+import '../../../../../domain/user/settings/settings.dart';
 import '../../../../../domain/user/user.dart';
 import 'state.dart';
 
@@ -38,8 +41,81 @@ class UserAvailabilityStatusCubit extends Cubit<UserAvailabilityStatusState> {
   late final _eventBus = dependencyLocator<EventBusObserver>();
   late final _colleagueRepository = dependencyLocator<ColleaguesRepository>();
   User? get _user => GetStoredUserUseCase()();
+  late final _changeSettings = ChangeSettingsUseCase();
+
+  /// We want the UI to be optimistic, rather than waiting for a server response
+  /// before changing the status. We set this temporarily while making changes
+  /// to immediately update the UI with the user's choice.
+  ColleagueAvailabilityStatus? _statusOverride;
+
+  Future<void> changeAvailabilityStatus(
+    ColleagueAvailabilityStatus requestedStatus,
+    List<Destination> destinations,
+  ) async {
+    _statusOverride = requestedStatus;
+    check();
+
+    final user = GetLoggedInUserUseCase()();
+
+    await _changeSettings(
+      user.settings.copyWithAll(
+        _determineSettingsToModify(user, destinations, requestedStatus),
+      ),
+    );
+
+    if (!HasFeature()(Feature.userBasedDnd)) {
+      // Hacky solution to making sure availability doesn't sometimes flick back
+      // to available. Should be removed with user-based dnd.
+      // ignore: inference_failure_on_instance_creation
+      await Future.delayed(const Duration(seconds: 1));
+      await _changeSettings(
+        user.settings.copyWith(
+          CallSetting.dnd,
+          requestedStatus == ColleagueAvailabilityStatus.doNotDisturb,
+        ),
+      );
+    }
+
+    _statusOverride = null;
+    check();
+  }
+
+  Map<SettingKey, Object> _determineSettingsToModify(
+    User user,
+    List<Destination> destinations,
+    ColleagueAvailabilityStatus requestedStatus,
+  ) {
+    final destination =
+        destinations.findHighestPriorityDestinationFor(user: user);
+
+    return switch (requestedStatus) {
+      ColleagueAvailabilityStatus.available => {
+          CallSetting.dnd: false,
+          if (destination != null) CallSetting.destination: destination,
+        },
+      ColleagueAvailabilityStatus.doNotDisturb => {
+          CallSetting.dnd: true,
+          if (destination != null) CallSetting.destination: destination,
+        },
+      ColleagueAvailabilityStatus.offline => {
+          CallSetting.dnd: false,
+          CallSetting.destination: const Destination.notAvailable(),
+        },
+      _ => throw ArgumentError(
+          'Only [available], [doNotDisturb], [offline] '
+          'are valid options for setting user status.',
+        ),
+    };
+  }
 
   Future<void> check([AvailabilityUpdate? availability]) async {
+    final override = _statusOverride;
+
+    if (override != null) {
+      emit(UserAvailabilityStatusState(status: override));
+      return;
+    }
+
     if (availability != null && HasFeature()(Feature.userBasedDnd)) {
       emit(
         UserAvailabilityStatusState(status: availability.availabilityStatus),
