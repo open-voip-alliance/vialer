acdb5d95f5e3324ccb78a7dddf54ca2ae34e5cd3
Jeremy Norman <jeremy.norman@wearespindle.com>
2023-06-27 16:34:10 +0200

    aqwweqwe

--- lib/domain/feature/has_feature.dart
+++ lib/domain/feature/has_feature.dart
@@ -1,14 +1,7 @@
-import 'package:recase/recase.dart';
-
-import '../../dependency_locator.dart';
-import '../env.dart';
 import '../use_case.dart';
 import 'feature.dart';
 
 /// Returns true if the given feature is enabled.
 class HasFeature extends UseCase {
-  final _env = dependencyLocator<EnvRepository>();
-
-  bool call(Feature feature) =>
-      _env.get('FEATURE_${feature.name.constantCase}').isNotEmpty;
+  bool call(Feature feature) => true;
 }
