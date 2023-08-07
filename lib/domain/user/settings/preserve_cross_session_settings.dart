65c546aaad597ae56186ba5942dec80b193591b5
Jeremy Norman <jeremy.norman@wearespindle.com>
2023-08-07 17:41:08 +0200

    More settings will now be remembered between sessions

---
+++ lib/domain/user/settings/preserve_cross_session_settings.dart
@@ -0,0 +1,50 @@
+import 'package:vialer/app/util/loggable.dart';
+import 'package:vialer/domain/use_case.dart';
+import 'package:vialer/domain/user/settings/settings.dart';
+
+import '../../../dependency_locator.dart';
+import '../../legacy/storage.dart';
+import '../user.dart';
+import 'app_setting.dart';
+import 'call_setting.dart';
+
+/// Will preserve any settings found in [preserve] for future sessions, so when
+/// the user logs back in.
+///
+/// This uses [StorageRepository] so `StorageRepository.clear()` must be called
+/// *before* executing this.
+class PreserveCrossSessionSettings extends UseCase with Loggable {
+  final _storage = dependencyLocator<StorageRepository>();
+
+  /// Must not include any user-specific settings, should almost always only be
+  /// [bool] or otherwise very simple settings.
+  ///
+  /// For example, it should NOT store [CallSetting.
+  static const preserve = [
+    CallSetting.usePhoneRingtone,
+    AppSetting.enableT9ContactSearch,
+    AppSetting.remoteLogging,
+    AppSetting.showCallsInNativeRecents,
+    AppSetting.showClientCalls,
+    AppSetting.showOnlineColleaguesOnly,
+    AppSetting.showTroubleshooting,
+  ];
+
+  Future<void> call(User user) async {
+    final settings = Settings(
+      Map.fromEntries(
+        preserve.map(
+          // This fails to compile without explicitly casting to [SettingKey]
+          // ignore: unnecessary_cast
+          (key) => MapEntry(key as SettingKey, user.settings.get(key)),
+        ),
+      ),
+    );
+
+    logger.info(
+      'Preserving [${preserve.map((e) => e.name).join(', ')}] for future login',
+    );
+
+    _storage.previousSessionSettings = settings;
+  }
+}
