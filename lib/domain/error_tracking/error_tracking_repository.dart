cd459717f42c79162fc4a908e3926c6b47afd44c
Jeremy Norman <jeremy.norman@wearespindle.com>
2023-08-10 10:21:50 +0200

    Print errors to remote logging as well as Sentry

--- lib/domain/error_tracking/error_tracking_repository.dart
+++ lib/domain/error_tracking/error_tracking_repository.dart
@@ -1,8 +1,13 @@
+import 'package:dartx/dartx.dart';
+import 'package:logging/logging.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
+import 'package:vialer/app/util/loggable.dart';
 
+import '../../dependency_locator.dart';
+import '../env.dart';
 import '../user/user.dart';
 
-class ErrorTrackingRepository {
+class ErrorTrackingRepository with Loggable {
   Future<void> run(
     void Function() appRunner,
     String dsn,
@@ -11,10 +16,36 @@ class ErrorTrackingRepository {
     await SentryFlutter.init(
       (options) => options
         ..dsn = dsn
-        ..beforeSend = (event, {dynamic hint}) => event.copyWith(
-              user: SentryUser(id: user?.uuid),
-            ),
+        ..beforeSend = (event, {dynamic hint}) {
+          logger.logSentryEvent(event);
+          return event.copyWith(user: SentryUser(id: user?.uuid));
+        },
       appRunner: appRunner,
     );
+  }
+}
+
+extension on Logger {
+  void logSentryEvent(SentryEvent event) => severe(
+        // The event id lets you look this up in Sentry so we don't need
+        // to display the entire stack trace.
+        'Error [${event.sentryUrl}]: ${event.title}',
+      );
+}
+
+extension on SentryEvent {
+  String get title => exceptions?.firstOrNull?.value ?? 'unknown';
+
+  EnvRepository get env => dependencyLocator<EnvRepository>();
+
+  String get baseUrl =>
+      // Extracts our sentry URL from the [errorTrackingDsn] env var so we don't
+      // need to repeat it.
+      RegExp('@([a-z\.]+)\/').firstMatch(env.errorTrackingDsn)?.group(0) ?? '';
+
+  String get sentryUrl {
+    final project = env.isProduction ? 'vialer-app' : 'vialer-beta-environment';
+
+    return 'https://${baseUrl}organizations/sentry/discover/$project:$eventId/';
   }
 }
