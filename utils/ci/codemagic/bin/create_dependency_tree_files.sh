06de510138b20a949fb42df5b52241e35e44e9ad
Jeremy Norman <jeremy.norman@wearespindle.com>
2023-08-15 16:39:49 +0200

    Added a codemagic workflow

--- utils/ci/codemagic/bin/create_dependency_tree_files.sh
+++ utils/ci/codemagic/bin/create_dependency_tree_files.sh
@@ -1,12 +1,14 @@
 DIRECTORY="$CM_EXPORT_DIR/dependency_trees"
 FLUTTER_FILE="$DIRECTORY/flutter.txt"
 ANDROID_FILE="$DIRECTORY/android.txt"
+COMBINED_FILE="$DIRECTORY/combined.txt"
 mkdir -p "$DIRECTORY"
 touch "$FLUTTER_FILE"
 touch "$ANDROID_FILE"
+touch "$COMBINED_FILE"
 
 flutter pub deps > "$FLUTTER_FILE"
-pwd
 cd android || exit
-pwd
 ./gradlew app:dependencies > "$ANDROID_FILE"
+cd ../
+cat "$FLUTTER_FILE" "$ANDROID_FILE" > "$COMBINED_FILE"
