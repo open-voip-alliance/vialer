d1ffad59bcab11379fee4c18685fa73a2b80b4bf
Jeremy Norman <jeremy.norman@wearespindle.com>
2023-08-16 13:37:46 +0200

    Remove unnecessary dependencies being flagged as ad-sdks

---
+++ utils/ci/codemagic/bin/create_dependency_tree_files.sh
@@ -0,0 +1,14 @@
+DIRECTORY="$CM_BUILD_DIR/dependency_tree"
+FLUTTER_FILE="$DIRECTORY/flutter.txt"
+ANDROID_FILE="$DIRECTORY/android.txt"
+COMBINED_FILE="$DIRECTORY/combined.txt"
+mkdir -p "$DIRECTORY"
+touch "$FLUTTER_FILE"
+touch "$ANDROID_FILE"
+touch "$COMBINED_FILE"
+
+flutter pub deps > "$FLUTTER_FILE"
+cd android || exit
+./gradlew app:dependencies > "$ANDROID_FILE"
+cd ../
+cat "$FLUTTER_FILE" "$ANDROID_FILE" > "$COMBINED_FILE"
