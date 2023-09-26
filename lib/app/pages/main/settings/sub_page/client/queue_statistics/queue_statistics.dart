2e93e330b03816c40e7d3f66bf2ef2124ecaa897
Jeremy Norman <jeremy.norman@wearespindle.com>
2023-09-26 14:56:52 +0100

    Quque stats

--- lib/app/pages/main/settings/sub_page/client/queue_statistics/queue_statistics.dart
+++ lib/app/pages/main/settings/sub_page/client/queue_statistics/queue_statistics.dart
@@ -1,7 +1,12 @@
+import 'package:dartx/dartx.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:res_client/event.dart';
+import 'package:res_client/model.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:res_client/client.dart';
+import 'package:vialer/domain/feature/has_feature.dart';
 
+import '../../../../../../../domain/feature/feature.dart';
 import '../../../../../../../domain/user/get_logged_in_user.dart';
 import '../../../../../../../domain/user/user.dart';
 
@@ -21,28 +26,106 @@ class QueueStatistics extends _$QueueStatistics {
     return QueueStatisticsState.loading();
   }
 
-  void connect() {
+  void connect() async {
+    if (!hasFeature(Feature.queueStatistics)) return;
+
     final url = 'wss://resgate.prod.holodeck.spindle.dev';
-    final client = ResClient()
-      ..reconnect(Uri.parse(url))
-      ..auth(
-        'usertoken',
-        'login',
-        params: {'token': _user.token},
-      )
-      ..subscribe('dashboard.client.${_user.client.uuid}', null);
+    final client = await ResClient()
+      ..reconnect(Uri.parse(url));
+
+    await client.auth(
+      'usertoken',
+      'login',
+      params: {'token': _user.token},
+    );
+
+    return _onAuthenticated(client);
+  }
+
+  Future<void> _onAuthenticated(ResClient client) async {
+    await client.subscribe('dashboard.client.${_user.client.uuid}', null);
+
+    final initialData = client.get('dashboard.client.${_user.client.uuid}');
+
+    if (initialData?.item is ResCollection) {
+      final items = (initialData!.item as ResCollection).items;
+
+      for (final item in items) {
+        if (item is ResModel) {
+          _updateState(item.rid, item.toJson());
+        }
+      }
+    }
 
     client.events.listen((event) {
-      print("TEST123 $event");
+      if (event is ModelChangedEvent) {
+        _updateState(event.rid, event.newProps);
+      }
     });
+  }
+
+  void _updateState(String name, Map<String, dynamic> data) {
+    var stats = state.map(
+      loading: (_) => <String, IndividualQueueStatistics>{},
+      loaded: (loaded) => loaded.stats,
+    );
+
+    stats = Map.from(stats);
+
+    var queueStats = stats[name];
+
+    if (queueStats != null) {
+      final queueData = queueStats.asData()..addAll(data);
+      queueStats = IndividualQueueStatistics(queueData);
+    } else {
+      queueStats = IndividualQueueStatistics(data);
+    }
+
+    stats[name] = queueStats;
+
+    state = QueueStatisticsState.loaded(stats: stats);
+  }
+
+  void selectSpecificQueue(String selected) {
+    final loaded = this.state;
+
+    if (loaded is Loaded && loaded.stats.containsKey(selected)) {
+      state = loaded.copyWith(selectedQueue: selected);
+    }
   }
 }
 
 @freezed
 sealed class QueueStatisticsState with _$QueueStatisticsState {
+  const QueueStatisticsState._();
+
   const factory QueueStatisticsState.loading() = Loading;
   const factory QueueStatisticsState.loaded({
-    required int callersInQueue,
-    required int activeCalls,
+    @Default(null) String? selectedQueue,
+    required Map<String, IndividualQueueStatistics> stats,
   }) = Loaded;
+
+  IndividualQueueStatistics get selectedStatistics {
+    final self = this as Loaded;
+
+    final selectedQueue = self.selectedQueue ?? self.stats.keys.first;
+
+    return self.stats[selectedQueue]!;
+  }
+}
+
+class IndividualQueueStatistics {
+  IndividualQueueStatistics(this._data);
+
+  final Map<String, dynamic> _data;
+
+  Map<String, dynamic> asData() => _data;
+
+  int get callersInQueue => _fromKey('waiting_callers');
+  int get activeCalls => _fromKey('total_active_calls');
+  int get totalAgents => _fromKey('total_agents');
+  int get loggedInAgents => _fromKey('logged_in_agents');
+  int get dndAgents => _fromKey('dnd_agents');
+
+  int _fromKey(String key) => _data.getOrElse(key, () => 0) as int;
 }
--- lib/app/pages/main/settings/sub_page/client/queue_statistics/widget.dart
+++ lib/app/pages/main/settings/sub_page/client/queue_statistics/widget.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:vialer/app/pages/main/settings/sub_page/client/queue_statistics/queue_statistics.dart';
+import 'package:vialer/app/widgets/stylized_dropdown.dart';
 
 import '../../../cubit.dart';
 import '../../widget.dart';
+import '../../../../../../resources/theme.dart';
 
 class QueueStatisticsSubPage extends ConsumerWidget {
   const QueueStatisticsSubPage({super.key});
@@ -19,9 +21,123 @@ class QueueStatisticsSubPage extends ConsumerWidget {
       child: (_) {
         return state.map(
           loading: (_) => Text('Loading'),
-          loaded: (loaded) => Text('loaded'),
+          loaded: (state) => _Loaded(state),
         );
       },
+    );
+  }
+}
+
+class _Loaded extends ConsumerWidget {
+  const _Loaded(this.state);
+
+  final Loaded state;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final queueStatistics = ref.read(queueStatisticsProvider.notifier);
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      mainAxisSize: MainAxisSize.max,
+      children: [
+        Visibility(
+          visible: state.stats.keys.length <= 1,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: StylizedDropdown(
+              value: state.selectedQueue,
+              isExpanded: true,
+              onChanged: (selected) =>
+                  queueStatistics.selectSpecificQueue(selected!),
+              items: state.stats.keys
+                  .map(
+                    (queueName) => DropdownMenuItem<String>(
+                      child: Text(queueName),
+                    ),
+                  )
+                  .toList(),
+            ),
+          ),
+        ),
+        const SizedBox(height: 20),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: GridView.count(
+              crossAxisCount: 2,
+              mainAxisSpacing: 20,
+              crossAxisSpacing: 20,
+              children: [
+                _QueueStatisticLarge(
+                  title: 'Callers in the queue',
+                  number: state.selectedStatistics.callersInQueue.toString(),
+                  suffix: 'Waiting',
+                ),
+                _QueueStatisticLarge(
+                  title: 'Active calls',
+                  number: state.selectedStatistics.activeCalls.toString(),
+                  suffix: 'Calls',
+                ),
+                _QueueStatisticLarge(
+                  title: 'Online colleagues in call group',
+                  number: state.selectedStatistics.loggedInAgents.toString(),
+                  suffix: '/${state.selectedStatistics.totalAgents.toString()}',
+                ),
+                _QueueStatisticLarge(
+                  title: 'Colleagues on a call or do not disturb',
+                  number: state.selectedStatistics.dndAgents.toString(),
+                  suffix:
+                      '/${state.selectedStatistics.loggedInAgents.toString()}',
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _QueueStatisticLarge extends StatelessWidget {
+  const _QueueStatisticLarge({
+    required this.title,
+    required this.number,
+    required this.suffix,
+  });
+
+  final String title;
+  final String number;
+  final String suffix;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(border: Border.all(color: Colors.black)),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Text(title),
+          Container(
+            color: context.brand.theme.colors.grey7,
+            child: Padding(
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                children: [
+                  Text(
+                    number,
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(suffix),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
