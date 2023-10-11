3fd4ae54667bc645cf94da9cefb9a11c514eaeb0
Jeremy Norman <jeremy.norman@wearespindle.com>
2023-10-11 15:53:24 +0200

    Updated to use conventional commit format

--- utils/upgrade_flutter.dart
+++ utils/upgrade_flutter.dart
@@ -71,7 +71,7 @@ Future<void> main(List<String> arguments) async {
   print('⚙️ Comminting files to git');
   await _runProcess(
     'git',
-    ['commit', '-m', 'chore(flutter): upgraded to version $versionNumber'],
+    ['commit', '-m', 'chore(flutter): upgrade to version $versionNumber'],
   );
 
   print('⚙️ Pushing files');
