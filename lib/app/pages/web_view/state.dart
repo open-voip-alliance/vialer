c6514aed98eef2b1676b671a08f5c19798f2ea2d
Jeremy Norman <jeremy.norman@wearespindle.com>
2023-11-22 10:04:14 +0100

    qa changes

--- lib/app/pages/web_view/state.dart
+++ lib/app/pages/web_view/state.dart
@@ -4,15 +4,23 @@ part 'state.freezed.dart';
 
 @freezed
 sealed class WebViewState with _$WebViewState {
+  const WebViewState._();
   const factory WebViewState.loadingUrl() = LoadingUrl;
-  const factory WebViewState.loadedUrl({
-    required String url,
-  }) = LoadedUrl;
-  const factory WebViewState.loadedWebView({
-    required String url,
-  }) = LoadedWebView;
+  const factory WebViewState.loadedUrl({required String url}) = LoadedUrl;
+  const factory WebViewState.loadedWebView({required String url}) =
+      LoadedWebView;
   const factory WebViewState.loadPortalUrlError() = LoadPortalUrlError;
   const factory WebViewState.loadWebViewError({
     required String description,
   }) = LoadWebViewError;
+
+  bool get isLoaded => switch (this) {
+        LoadedUrl() || LoadedWebView() => true,
+        _ => false,
+      };
+
+  String get url => switch (this) {
+        LoadedUrl() || LoadedWebView() => url,
+        _ => '',
+      };
 }
