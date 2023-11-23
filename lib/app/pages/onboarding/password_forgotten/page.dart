c490712136fc1012ae030fd16a4029db63acaa90
Johannes Nevels <johannes.nevels@voys.nl>
2023-11-23 16:57:26 +0100

    Improved layout more according to design

--- lib/app/pages/onboarding/password_forgotten/page.dart
+++ lib/app/pages/onboarding/password_forgotten/page.dart
@@ -32,7 +32,7 @@ class PasswordForgottenPage extends ConsumerWidget {
         (PasswordForgottenState? previousState,
             PasswordForgottenState newState) {
       if (newState is Success)
-        _pop('Your password has been reset. Please check your email.');
+        _pop(context.msg.onboarding.passwordForgotten.success);
     });
 
     /// Handles the request for a new password.
@@ -50,7 +50,7 @@ class PasswordForgottenPage extends ConsumerWidget {
       }
     }
 
-    String _errorText(PasswordForgottenState passwordForgottenState) {
+    String _errorText() {
       if (passwordForgottenState is NotSubmitted &&
           !passwordForgottenState.hasValidEmailFormat) {
         return context.msg.onboarding.login.error.wrongEmailFormat;
@@ -65,57 +65,70 @@ class PasswordForgottenPage extends ConsumerWidget {
       body: Background(
         style: Style.split,
         child: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Align(
-                  alignment: Alignment.topRight,
+          child: Column(
+            children: [
+              Align(
+                alignment: Alignment.topRight,
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
                   child: IconButton(
-                      icon: Icon(Icons.close, color: Colors.white, size: 40),
-                      onPressed: () => _pop()),
+                    icon: Icon(Icons.close, color: Colors.white, size: 40),
+                    onPressed: () => _pop(),
+                  ),
                 ),
-                Wrap(
-                  runSpacing: 40,
-                  children: [
-                    title(),
-                    description(),
-                    emailInput(context, passwordForgottenState,
-                        _errorText(passwordForgottenState)),
-                  ],
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(50.0, 0.0, 50.0, 20.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Wrap(
+                        runSpacing: 40,
+                        children: [
+                          title(context),
+                          description(context),
+                          emailInput(
+                              context, passwordForgottenState, _errorText()),
+                        ],
+                      ),
+                      Spacer(),
+                      StylizedButton.raised(
+                          colored: true,
+                          onPressed: _handleRequestNewPassword,
+                          isLoading: passwordForgottenState is Loading,
+                          child: passwordForgottenState is Loading
+                              ? Text(context.msg.onboarding.passwordForgotten
+                                  .button.requestingPasswordReset)
+                              : Text(context.msg.onboarding.passwordForgotten
+                                  .button.requestPasswordReset)),
+                    ],
+                  ),
                 ),
-                Spacer(),
-                StylizedButton.raised(
-                    colored: true,
-                    onPressed: _handleRequestNewPassword,
-                    isLoading: passwordForgottenState is Loading,
-                    child: passwordForgottenState is Loading
-                        ? Text("Requesting password reset")
-                        : Text("Request password reset"))
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
     );
   }
 
-  Text title() {
-    return Text('Password forgotten',
-        textAlign: TextAlign.center,
-        style: const TextStyle(
-          fontSize: 40,
-          fontWeight: FontWeight.bold,
-          color: Colors.white,
-        ));
+  Center title(BuildContext context) {
+    return Center(
+      child: Text(context.msg.onboarding.passwordForgotten.title,
+          textAlign: TextAlign.center,
+          style: const TextStyle(
+            fontSize: 40,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          )),
+    );
   }
 
-  Text description() {
-    return Text(
-        'Enter your email address below to receive an email with the steps to reset your password.',
-        textAlign: TextAlign.center,
+  Text description(BuildContext context) {
+    return Text(context.msg.onboarding.passwordForgotten.description,
+        textAlign: TextAlign.left,
         style: const TextStyle(
           fontSize: 16,
           color: Colors.white60,
