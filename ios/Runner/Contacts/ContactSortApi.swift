c46a7d6c51d8475a9597bfe36b1956eb78fe65e1
Jeremy Norman <jeremy.norman@wearespindle.com>
2023-12-01 10:41:51 +0100

    chore(flutter): upgrade to version 3.16.0

--- ios/Runner/Contacts/ContactSortApi.swift
+++ ios/Runner/Contacts/ContactSortApi.swift
@@ -2,11 +2,11 @@ import Contacts
 
 class ContactSortApi: NSObject, ContactSortHostApi {
     func getSortingWithError(_ error: AutoreleasingUnsafeMutablePointer<FlutterError?>) -> ContactSort? {
-        let contactSort = switch CNContactsUserDefaults.shared().sortOrder {
-            case .givenName: OrderBy.givenName
-            case .familyName: OrderBy.familyName
-            default: OrderBy.givenName
-        }
+        let contactSort = {switch CNContactsUserDefaults.shared().sortOrder {
+        case .givenName: OrderBy.givenName
+        case .familyName: OrderBy.familyName
+        default: OrderBy.givenName
+        }}()
         
         return ContactSort.makeWithOrder(by: OrderByBox(value: contactSort))
     }
