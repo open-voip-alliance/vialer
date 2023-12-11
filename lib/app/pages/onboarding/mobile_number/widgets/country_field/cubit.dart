f1910e9247ca324022cca00ea08c6413b92482c2
Johannes Nevels <johannes.nevels@voys.nl>
2023-12-11 15:18:04 +0100

    feat: set current country based on device locale

--- lib/app/pages/onboarding/mobile_number/widgets/country_field/cubit.dart
+++ lib/app/pages/onboarding/mobile_number/widgets/country_field/cubit.dart
@@ -4,6 +4,7 @@ import 'package:collection/collection.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_libphonenumber/flutter_libphonenumber.dart';
+import 'package:country_codes/country_codes.dart';
 
 import '../../../../../../domain/onboarding/country.dart';
 import '../../../../../../domain/onboarding/get_countries.dart';
@@ -25,12 +26,40 @@ class CountryFieldCubit extends Cubit<CountryFieldState> {
       (country) => ['NL', 'DE', 'BE', 'ZA'].contains(country.isoCode),
     );
 
+    final totalCountries = [...mainCountries, ...countries];
+
     emit(
       CountriesLoaded(
-        countries: [...mainCountries, ...countries],
-        currentCountry: mainCountries.first,
+        countries: totalCountries,
+        currentCountry:
+            await _getPreferredCurrentCountry(countries: totalCountries),
       ),
     );
+  }
+
+  /// Retrieves the preferred country from a list of countries.
+  ///
+  /// The preferred country is determined by the current locale of the device.
+  /// It uses the `CountryCodes` library to initialize the country codes and
+  /// retrieve the ISO code of the current locale. Then, it searches for the
+  /// country in the provided list of countries that matches the ISO code.
+  /// If no match is found, it returns the first country in the list.
+  ///
+  /// Parameters:
+  /// - `countries`: The list of countries to search from.
+  ///
+  /// Returns:
+  /// The preferred country.
+  Future<Country> _getPreferredCurrentCountry(
+      {required Iterable<Country> countries}) async {
+    await CountryCodes.init();
+    final String? countryIsoCode = CountryCodes.detailsForLocale().alpha2Code;
+    Country preferredCountry = countries.firstWhere(
+      (Country country) => country.isoCode == countryIsoCode,
+      orElse: () => countries.first,
+    );
+
+    return preferredCountry;
   }
 
   Future<void> pickCountryByMobileNumber(String mobileNumber) async {
