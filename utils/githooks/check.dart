fec5a40a687cb4c9fc275a9b896e3b6bb80c9086
Jeremy Norman <jeremynorman89@gmail.com>
2024-01-17 18:21:34 +0100

    chore: removed Pigeon.kt from git #1896

--- utils/githooks/check.dart
+++ utils/githooks/check.dart
@@ -203,24 +203,8 @@ Future<void> _runGenerationIfNeeded(
   );
 
   final pigeonProcess = await Process.start(
-    'flutter',
-    [
-      'pub',
-      'run',
-      'pigeon',
-      '--input',
-      'utils/pigeon/scheme.dart',
-      '--dart_out',
-      'lib/app/util/pigeon.dart',
-      '--objc_header_out',
-      'ios/Runner/pigeon.h',
-      '--objc_source_out',
-      'ios/Runner/pigeon.m',
-      '--kotlin_out',
-      'android/app/src/main/kotlin/com/voipgrid/vialer/Pigeon.kt',
-      '--kotlin_package',
-      'com.voipgrid.vialer',
-    ],
+    '.',
+    ['utils/bin/pigeon.sh'],
   );
 
   await _writeAndExitIfNotZero(
