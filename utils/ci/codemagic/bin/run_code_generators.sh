4427fa6ef9aae4e5cad22f685e15f8044013876e
Jeremy Norman <jeremynorman89@gmail.com>
2024-01-17 18:12:14 +0100

    chore: removed Pigeon.kt from git #1896

--- utils/ci/codemagic/bin/run_code_generators.sh
+++ utils/ci/codemagic/bin/run_code_generators.sh
@@ -5,9 +5,4 @@ dart pub global activate onepub
 onepub import
 flutter packages pub get
 . "$CM_BUILD_DIR"/utils/bin/strings.sh
-dart run pigeon --input utils/pigeon/scheme.dart \
-  --dart_out lib/app/util/pigeon.dart \
-  --objc_header_out ios/Runner/pigeon.h \
-  --objc_source_out ios/Runner/pigeon.m \
-  --kotlin_out android/app/src/main/kotlin/com/voipgrid/vialer/Pigeon.kt \
-  --kotlin_package com.voipgrid.vialer
+. "$CM_BUILD_DIR"/utils/bin/pigeon.sh
