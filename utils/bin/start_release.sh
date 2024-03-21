a7a44a49b215ca07c1683508b352fe534cee710f
Jeremy Norman <jeremynorman89@gmail.com>
2024-03-21 15:20:58 +0100

    chore: added release notes for v7.50.0

--- utils/bin/start_release.sh
+++ utils/bin/start_release.sh
@@ -14,27 +14,21 @@ if [[ ! $version =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
     exit 1
 fi
 
-## Checkout develop branch
-#git checkout develop
-#
-## Pull latest changes from origin
-#git pull origin develop
-#
-## Checkout new release branch
-#release_branch="release/$version"
-#git checkout -b "$release_branch"
-#
-## Find most recent release notes directory
-#recent_release_notes=$(ls -dtr release_notes/*/ | tail -n 1)
-#
-## Create new directory within release_notes
+git checkout develop
+git pull origin develop
+release_branch="release/$version"
+git checkout -b "$release_branch"
+recent_release_notes=$(ls -dtr release_notes/*/ | tail -n 1)
 new_directory="release_notes/$version"
-#mkdir -p "$new_directory"
-#cp -r "$recent_release_notes"/* "$new_directory"
-#
-## Add new directory to git
-#git add "$new_directory"
+mkdir -p "$new_directory"
+cp -r "$recent_release_notes"/* "$new_directory"
+git add "$new_directory"
 
-
-echo -e "\n\033[1;33mSuggested commit message:\033[0m \033[1;36mchore:\033[0m added release notes for \033[1;32m$version\033[0m"
-echo -e "⚠️⚠️ \033[1;31mRemember to edit the release notes file in $new_directory.\033[0m ⚠️⚠️"
+echo -e "\033[1;33mThe release notes directory has been created and populated with the most recent release notes.\033[0m\n"
+echo -e "\033[1;33mHere are the next actions:\033[0m\n"
+echo -e "1. \033[1;32mEdit $version/english.txt with the new release notes\033[0m\n"
+echo -e "2. \033[1;32mCommit these release notes, including the previous suggested commit message formatted appropriately\033[0m"
+echo -e "\033[1;33mSuggested commit message:\033[0m \033[1;36mchore: added release notes for $version\n"
+echo -e "3. \033[1;32mPush the changes to $release_branch"
+echo -e "\033[1;33mSuggested command:\033[0m \033[1;36mgit push origin $release_branch\n"
+echo -e "\033[1;33mOnce the branch has been pushed, the release candidate should begin building.\033[0m\n"
