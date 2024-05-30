24b737c68791fb17be5f35b584d4394cdb979750
Jeremy Norman <jeremynorman89@gmail.com>
2024-05-30 11:35:21 +0200

    feat: expanded dialer button touch area to use available space #1975

--- lib/presentation/shared/widgets/dial_pad/keypad.dart
+++ lib/presentation/shared/widgets/dial_pad/keypad.dart
@@ -365,25 +365,25 @@ class _KeypadValueButtonState extends State<KeypadValueButton> {
                 .join()
             : widget.secondaryValue;
 
-    return Center(
-      child: ConstrainedBox(
-        constraints: const BoxConstraints(
-          maxWidth: KeypadValueButton.maxSize,
-          maxHeight: KeypadValueButton.maxSize,
-        ),
-        child: LayoutBuilder(
-          builder: (context, constraints) {
-            return KeypadButton(
-              child: Semantics(
-                label: widget.primaryValue,
-                hint: secondaryValueSemanticsLabel,
-                child: _InkWellOrResponse(
-                  response: !context.isIOS,
-                  customBorder: const CircleBorder(),
-                  onTapDown: _enterValue,
-                  onLongPress: widget.replaceWithSecondaryValueOnLongPress
-                      ? _replaceWithSecondaryValue
-                      : null,
+    return _InkWellOrResponse(
+      response: !context.isIOS,
+      customBorder: const CircleBorder(),
+      onTapDown: _enterValue,
+      onLongPress: widget.replaceWithSecondaryValueOnLongPress
+          ? _replaceWithSecondaryValue
+          : null,
+      child: Center(
+        child: ConstrainedBox(
+          constraints: const BoxConstraints(
+            maxWidth: KeypadValueButton.maxSize,
+            maxHeight: KeypadValueButton.maxSize,
+          ),
+          child: LayoutBuilder(
+            builder: (context, constraints) {
+              return KeypadButton(
+                child: Semantics(
+                  label: widget.primaryValue,
+                  hint: secondaryValueSemanticsLabel,
                   child: Column(
                     mainAxisSize: MainAxisSize.min,
                     mainAxisAlignment: MainAxisAlignment.center,
@@ -418,9 +418,9 @@ class _KeypadValueButtonState extends State<KeypadValueButton> {
                     ],
                   ),
                 ),
-              ),
-            );
-          },
+              );
+            },
+          ),
         ),
       ),
     );
