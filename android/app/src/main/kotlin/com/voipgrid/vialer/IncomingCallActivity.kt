e8e474bfcab60f64ff6d5f89472d5d40bdd122ff
Jeremy Norman <jeremynorman89@gmail.com>
2024-08-06 18:59:15 +0100

    fix: using exported #2011

--- android/app/src/main/kotlin/com/voipgrid/vialer/IncomingCallActivity.kt
+++ android/app/src/main/kotlin/com/voipgrid/vialer/IncomingCallActivity.kt
@@ -58,7 +58,7 @@ class IncomingCallActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        registerReceiver(receiver, IntentFilter(INCOMING_CALL_CANCEL_INTENT), ContextCompat.RECEIVER_NOT_EXPORTED)
+        ContextCompat.registerReceiver(this, receiver, IntentFilter(INCOMING_CALL_CANCEL_INTENT), ContextCompat.RECEIVER_EXPORTED)
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         // Although the title is not shown visually, we still set it for screen readers.
         title = getString(R.string.main_call_incoming_subtitle_with, getString(R.string.app_name))
